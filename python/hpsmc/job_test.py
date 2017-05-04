ee4bab2aa959775d280f2e5de5bfd3dc5cf1757e
JeremyMcCormick <jermccormick@gmail.com>
2017-05-04 08:30:47 -0700

    minor change to job test

--- python/hpsmc/job_test.py
+++ python/hpsmc/job_test.py
@@ -5,11 +5,7 @@ from hpsmc.generators import EGS5
 from hpsmc.base import Job
 from hpsmc.tools import StdHepTool
 
-rp = RunParameters(key="1pt05")
-
-egs5 = EGS5(name="moller_v3", bunches=5000)
-egs5.run_params = rp 
-
+egs5 = EGS5(name="moller_v3", bunches=5000, run_params=RunParameters(key="1pt05"))
 stdhep_print = StdHepTool(name="print_stdhep", args=["10"], inputs=["moller.stdhep"])
 
 job = Job(name="EGS5 Test", components=[egs5, stdhep_print]) 
