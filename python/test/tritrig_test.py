5c7b2b855b6d519d13b608e64ae27d57970e4ef5
JeremyMcCormick <jermccormick@gmail.com>
2017-05-09 13:05:17 -0700

    add stdhep converter to tritrig test job

--- python/test/tritrig_test.py
+++ python/test/tritrig_test.py
@@ -1,9 +1,13 @@
-from hpsmc.generators import MG4
+from hpsmc.generators import MG4, StdHepConverter
+from hpsmc.run_params import RunParameters 
 from hpsmc.base import Job
 
 mg4 = MG4(name="tritrig", run_card="run_card_1pt05.dat", outputs=["tritrig_test"])
 
-job = Job(name="tritrig test", components=[mg4])
+stdhep_cnv = StdHepConverter(run_params=RunParameters(key="1pt05"),
+    inputs=["./MG_mini_Tri_W/apTri/SubProcesses/events.lhe"])
+
+job = Job(name="tritrig test", components=[mg4, stdhep_cnv])
 job.setup()
 job.run()
 job.cleanup()
