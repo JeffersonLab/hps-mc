687132e08bb8e4e15f88c7c312e178503aa491e8
JeremyMcCormick <jermccormick@gmail.com>
2017-05-08 20:41:58 -0700

    use env variable to point to run params data

--- python/hpsmc/run_params.py
+++ python/hpsmc/run_params.py
@@ -5,8 +5,7 @@ class RunParameters:
     def __init__(self, key, json_filename = None):
         self.key = key
         if json_filename is None:
-            json_filename = os.path.dirname(os.path.realpath(sys.argv[0])) \
-                    + "/../../data/run_params.json"
+            json_filename = os.path.join(os.environ["HPSMC_DATA_DIR"], "run_params.json")
         rawdata = open(json_filename, 'r').read()
         self.json_dict = json.loads(rawdata)
 
