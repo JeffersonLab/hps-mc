dc462cc63747d5af01b174c556429a6a90edcd63
JeremyMcCormick <jermccormick@gmail.com>
2017-05-09 19:56:14 -0700

    add generic handling of JVM args; add python wrapper for DST Maker

--- python/hpsmc/tools.py
+++ python/hpsmc/tools.py
@@ -20,15 +20,12 @@ class SLIC(Component):
     def __init__(self, **kwargs):
         self.name = "slic"
         Component.__init__(self, **kwargs)
-        self.name = "slic"
         self.command = self.name
         if "detector" in kwargs:
             self.detector = kwargs["detector"]
         else:
             raise Exception("Missing detector argument for SLIC.")
-        if "nevents" in kwargs:
-            self.nevents = kwargs["nevents"]
-        else:
+        if self.nevents == -1:
             self.nevents = 999999999
  
     def cmd_args(self):
@@ -44,10 +41,10 @@ class SLIC(Component):
         if not os.path.exists("./fieldmap"):
             os.symlink(os.environ["HPSMC_FIELDMAPS_DIR"], "fieldmap")
 
-class HPSJava(Component):
+class JobManager(Component):
 
     def __init__(self, **kwargs):
-        self.name = "hps-java"
+        self.name = "HPS Java Job Manager"
         Component.__init__(self, **kwargs)
         self.command = "java"
         if "steering_resource" in kwargs:
@@ -60,11 +57,17 @@ class HPSJava(Component):
             self.defs = kwargs["defs"]
         else:
             self.defs = {}
+        if "java_args" in kwargs:
+            self.java_args = kwargs["java_args"]
+        else:
+            self.java_args = ["-Xmx500m", "-XX:+UseSerialGC"]
 
     def cmd_args(self):
         if not len(self.inputs):
             raise Exception("No inputs provided to hps-java.")
-        self.args = ["-Xmx2g", "-jar", os.environ["HPSJAVA_JAR"]]
+        self.args.extend(self.java_args)
+        self.args.append("-jar")
+        self.args.append(os.environ["HPSJAVA_JAR"])
         self.args.append("-i")
         self.args.append(self.inputs[0])
         if len(self.outputs):
@@ -79,3 +82,93 @@ class HPSJava(Component):
         elif self.steering_file is not None:
             self.args.append(self.steering_file)
         return self.args
+    
+class JavaTool(Component):
+    
+    def __init__(self, **kwargs):
+        self.name = "HPS Java Tool"
+        Component.__init__(self, **kwargs)
+        self.command = "java"
+        if "java_args" in kwargs:
+            self.java_args = kwargs["java_args"]
+        else:
+            self.java_args = ["-Xmx500m", "-XX:+UseSerialGC"]
+        if "java_class" in kwargs:
+            self.java_class = kwargs["java_class"]
+        elif self.java_class is None:
+            raise Exception("Missing java_class argument for JavaTool.")
+            
+    def cmd_args(self):
+        orig_args = self.args
+        self.args = []
+        self.args.extend(self.java_args)
+        self.args.append("-cp")
+        self.args.append(os.environ["HPSJAVA_JAR"])
+        self.args.append(self.java_class)
+        self.args.extend(orig_args)
+        return self.args
+    
+class FilterMCBunches(JavaTool):
+    
+    def __init__(self, **kwargs):
+        self.name = "Filter MC Bunches"
+        self.java_class = "org.hps.util.FilterMCBunches"
+        JavaTool.__init__(self, **kwargs)
+        if "ecal_hit_ecut" in kwargs:
+            self.ecal_hit_ecut = kwargs["ecal_hit_ecut"]
+        else:
+            self.ecal_hit_ecut = None
+        if "event_interval" in kwargs:
+            self.event_interval = kwargs["event_interval"]
+        else:
+            raise Exception("Missing required event_interval arg for FilterMCBunches.")
+        if "enable_ecal_energy_filter" in kwargs:
+            self.enable_ecal_energy_filter = kwargs["enable_ecal_energy_filter"]
+        else:
+            self.enable_ecal_energy_filter = False 
+        if self.nevents == -1:
+            raise Exception("Missing required nevents arg for FilterMCBunches.")
+                    
+    def cmd_args(self):
+        if not len(self.inputs):
+            raise Exception("Missing required inputs for FilterMCBunches.")
+        if not len(self.outputs):
+            raise Exception("Missing required outputs for FilterMCBunches.")
+        orig_args = self.args
+        self.args = JavaTool.cmd_args(self)
+        self.args.append("-e")
+        self.args.append(str(self.event_interval))
+        for i in self.inputs:
+            self.args.append(i)
+        self.args.append(self.outputs[0])
+        if self.enable_ecal_energy_filter:
+            self.args.append("-d")
+        if self.ecal_hit_ecut is not None:
+            self.args.append("-E")
+            self.args.append(str(self.ecal_hit_ecut))
+        self.args.append("-w")
+        self.args.append(str(self.nevents))
+        return self.args
+
+class DST(Component):
+ 
+    def __init__(self, **kwargs):
+        self.name = "HPS DST Maker"
+        self.command = "dst_maker"
+        Component.__init__(self, **kwargs)
+        
+    def cmd_args(self):
+        if not len(self.outputs):
+            raise Exception("Missing required outputs for DST.")
+        if not len(self.inputs):
+            raise Exception("Missing required inputs for DST.")
+        self.args = []
+        self.args.append("-o")
+        self.args.append(self.outputs[0])
+        if self.nevents != -1:
+            self.args.append("-n")
+            self.args.append(str(self.nevents))
+        for i in self.inputs:
+            self.args.append(i)
+        return self.args
+                
