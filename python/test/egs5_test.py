387d980ed5336702ba3319a97097b4edfb9d5164
JeremyMcCormick <jermccormick@gmail.com>
2017-05-09 19:57:30 -0700

    add outputs argument

--- python/test/egs5_test.py
+++ python/test/egs5_test.py
@@ -1,13 +1,13 @@
-from hpsmc import run_params
-from run_params import RunParameters
-from generators import EGS5
-from hpsmc.base import Job
-
 import sys
+
+from hpsmc.base import Job
+from hpsmc.generators import EGS5
+from hpsmc.run_params import RunParameters
 
 egs5 = EGS5(name="moller_v3",
     bunches=5000,
-    run_params=RunParameters(key="1pt05"))
+    run_params=RunParameters(key="1pt05"),
+    outputs=["events.stdhep"])
 
 job = Job(name="EGS5 Test", components=[egs5])
 job.setup()
