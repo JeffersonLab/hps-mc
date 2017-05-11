28e890c4418517defea0f6efc4c3d4ece2fac304
JeremyMcCormick <jermccormick@gmail.com>
2017-05-10 20:02:54 -0700

    modify job after changes to MG4 output

--- python/test/job_test.py
+++ python/test/job_test.py
@@ -9,12 +9,13 @@ from hpsmc.tools import SLIC, JobManager, FilterMCBunches, DST
 mg4 = MG4(description="Generate tritrig events using MG4",
           name="tritrig",
           run_card="run_card_1pt05.dat",
-          outputs=["tritrig"])
+          outputs=["tritrig"],
+          nevents=1000)
 
 # convert LHE output to stdhep
 stdhep_cnv = StdHepConverter(description="Convert LHE events to StdHep using EGS5",
                              run_params=RunParameters(key="1pt05"),
-                             inputs=["tritrig.lhe"],
+                             inputs=["tritrig_events.lhe.gz"],
                              outputs=["tritrig.stdhep"])
 
 # generate events in slic
