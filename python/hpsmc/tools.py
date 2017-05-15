bfbabf363c9ed92e2c3a5ac397a761c9d8ab0fab
JeremyMcCormick <jermccormick@gmail.com>
2017-05-15 11:38:55 -0700

    automatically use alternate db connection when running at SLAC

--- python/hpsmc/tools.py
+++ python/hpsmc/tools.py
@@ -1,4 +1,4 @@
-import os
+import os, socket
 
 from hpsmc.base import Component
 
@@ -61,6 +61,8 @@ class JobManager(Component):
             self.java_args = kwargs["java_args"]
         else:
             self.java_args = ["-Xmx500m", "-XX:+UseSerialGC"]
+        if "slac.stanford.edu" in socket.getfqdn():
+            self.java_args.append("-Dorg.hps.conditions.connection.resource=/org/hps/conditions/config/slac_connection.prop")
 
     def cmd_args(self):
         if not len(self.inputs):
