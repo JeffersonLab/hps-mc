58dd75ddead007580900bd29d12843b141c5c4dc
JeremyMcCormick <jermccormick@gmail.com>
2017-05-19 18:01:16 -0700

    set the seed to confirm it gets set in run card; remove comment

--- python/test/mg4_test.py
+++ python/test/mg4_test.py
@@ -8,17 +8,11 @@ ap = MG4(description="Generate A-prime events with APMASS param",
          run_card="run_card_1pt05.dat",
          params={"APMASS": 40.0},
          outputs=["ap"],
-         nevents=1000)
+         rand_seed=1234,
+         nevents=5000)
 
 unzip = Unzip(inputs=["ap_events.lhe.gz"])
 
-"""
-Executes the equivalent of this command:
-
-stdhep_lhe_tridents_displacetime ap_events.lhe ap.stdhep -s1 -l1
-
-This should displace the AP by ~1 mm by setting the proper decay time.
-"""
 rand_seed = 1
 z = 1.0
 displ = StdHepTool(name="lhe_tridents_displacetime",
