88c952505b1ea580ae0eb3bb84fc3440edebe7dc
JeremyMcCormick <jermccormick@gmail.com>
2017-06-05 16:54:26 -0700

    add EGS5 beam_v3 job script

---
+++ python/jobs/egs5-beam-v3_job.py
@@ -0,0 +1,17 @@
+from hpsmc.job import Job
+from hpsmc.generators import EGS5
+from hpsmc.run_params import RunParameters
+
+job = Job(name="EGS5 beam_v3 job")
+job.initialize()
+
+params = job.params
+
+egs5 = EGS5(name="beam_v3",
+    bunches=params.bunches,
+    run_params=RunParameters(key="1pt05"),
+    outputs=["beam.stdhep"])
+
+job.components = [egs5]
+
+job.run()
