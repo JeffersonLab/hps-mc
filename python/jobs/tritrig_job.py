16dd31939a28bc0d936bc3d5c7f117743eae23fa
JeremyMcCormick <jermccormick@gmail.com>
2017-06-06 22:42:12 -0700

    add stdhep for add_mother stdhep tool

--- python/jobs/tritrig_job.py
+++ python/jobs/tritrig_job.py
@@ -19,35 +19,35 @@ filename = params.filename
 procname = "tritrig"
 
 # generate tritrig in MG5
-mg = MG5(description="Generate tritrig events using MG5",
-         name=procname,
+mg = MG5(name=procname,
          run_card="run_card_"+params.run_params+".dat",
          outputs=[filename],
          nevents=params.nevents)
 
 # convert LHE output to stdhep
-stdhep_cnv = StdHepConverter(description="Convert LHE events to StdHep using EGS5",
-                             run_params=RunParameters(key=params.run_params),
+stdhep_cnv = StdHepConverter(run_params=RunParameters(key=params.run_params),
                              inputs=[procname+"_events.lhe.gz"],
                              outputs=[procname+".stdhep"])
 
-# rotate events into beam coords
-rot = StdHepTool(description="Rotate events into beam coords",
-                 name="beam_coords",
+# add mother particle to tag trident particles
+mom = StdHepTool(name="add_mother",
                  inputs=[procname+".stdhep"],
+                 outputs=[procname+"_mom.stdhep"])
+
+# rotate events into beam coords
+rot = StdHepTool(name="beam_coords",
+                 inputs=[procname+"_mom.stdhep"],
                  outputs=[procname+"_rot.stdhep"])
 
 # generate events in slic
-slic = SLIC(description="Run detector simulation using SLIC",
-            detector=params.detector,
+slic = SLIC(detector=params.detector,
             inputs=[procname+"_rot.stdhep"], 
             outputs=[procname+".slcio"], 
             nevents=params.nevents,
             ignore_returncode=True)
 
 # insert empty bunches expected by pile-up simulation
-filter_bunches = FilterMCBunches(description="Run utility to space out events",
-                                 java_args=["-DdisableSvtAlignmentConstants"],
+filter_bunches = FilterMCBunches(java_args=["-DdisableSvtAlignmentConstants"],
                                  inputs=[procname+".slcio"],
                                  outputs=[procname+"_filt.slcio"],
                                  ecal_hit_ecut=0.05,
@@ -56,22 +56,19 @@ filter_bunches = FilterMCBunches(description="Run utility to space out events",
                                  event_interval=250)
 
 # run simulated events in readout to generate triggers
-readout = JobManager(description="Run the readout simulation to create triggers",
-                     steering_resource=params.readout_steering,
+readout = JobManager(steering_resource=params.readout_steering,
                      java_args=["-DdisableSvtAlignmentConstants"],
                      defs={"detector": params.detector, "run": params.run},
                      inputs=[procname+"_filt.slcio"],
                      outputs=[procname+"_readout"])
 
 # run physics reconstruction
-recon = JobManager(description="Run the MC recon",
-                   steering_resource=params.recon_steering,
+recon = JobManager(steering_resource=params.recon_steering,
                    java_args=["-DdisableSvtAlignmentConstants"],
                    defs={"detector": params.detector, "run": params.run},
                    inputs=[procname+"_readout.slcio"],
                    outputs=[filename+"_recon"])
-                        
  
 # run the job
-job.components=[mg, stdhep_cnv, rot, slic, filter_bunches, readout, recon] 
+job.components=[mg, stdhep_cnv, mom, rot, slic, filter_bunches, readout, recon] 
 job.run()
