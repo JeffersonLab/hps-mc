952935813cbf471251feff095b363c82f762e652
JeremyMcCormick <jermccormick@gmail.com>
2017-06-30 17:52:05 -0700

    add job script for concatenating lcio files

---
+++ python/jobs/lcio-concat_job.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Job for concatenating LCIO files together.
+"""
+
+from hpsmc.job import Job
+from hpsmc.tools import LCIOConcat
+
+job = Job(name="LCIO concat job")
+job.initialize()
+
+input_files = job.params.input_files.keys()
+
+if len(input_files) < 2:
+    raise Exception("Not enough input files were provided.")
+
+print input_files
+
+concat = LCIOConcat(inputs=input_files, outputs=["events.slcio"])
+
+job.components = [concat]
+
+job.run()
