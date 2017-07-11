567386d87fff8ba71b1eb8dc38a1c6a89a81683c
JeremyMcCormick <jermccormick@gmail.com>
2017-07-10 17:37:34 -0700

    improve automatic file naming so it extracts the file numbers when no output mapping is provided

--- python/jobs/lcio-concat_job.py
+++ python/jobs/lcio-concat_job.py
@@ -2,11 +2,20 @@
 
 """
 Job for concatenating LCIO files together.
+
+This script ignores the provided output destination and automatically names the output file
+from the input source with the sequence numbers of the input files processed appended.
 """
 
 import os
 from hpsmc.job import Job
 from hpsmc.tools import LCIOConcat
+
+def split_file_number(filename):
+    basefile = os.path.basename(os.path.splitext(filename)[0])
+    file_number = basefile[basefile.rfind('_')+1:]
+    basefile = basefile[:basefile.rfind('_')]
+    return basefile, file_number
 
 job = Job(name="LCIO concat job")
 job.initialize()
@@ -16,16 +25,18 @@ if len(input_files) < 2:
     raise Exception("Not enough input files were provided (at least 2 required).")
 
 """
-If the name of the output file and its destination are the same, then the file numbers
-used in the job are appended to it.  In the case that the destination file has a different
-name, it is assumed that the user wants to use that file name explicitly rather than have
-these numbers automatically added.
+If no output file mapping is provided explicitly, then an auto-naming scheme is used which
+concatenates the start and end file numbers onto the base name of the first input file.
 """
-output_src = job.params.output_files.keys()[0]
-if output_src == job.params.output_files[output_src]:
-    start_filenum = os.path.splitext(input_files[0])[1][1:]
-    end_filenum = os.path.splitext(input_files[-1])[1][1:]
-    output_basename = os.path.splitext(os.path.basename(output_src))[0]
+if not len(job.params.output_files):
+    
+    output_basename,start_filenum = split_file_number(job.params.input_files[input_files[0]])
+    dontcare,end_filenum = split_file_number(job.params.input_files[input_files[-1]])
+    
+    print output_basename
+    print start_filenum
+    print end_filenum
+    
     try:
         int(start_filenum)
     except:
@@ -34,11 +45,12 @@ if output_src == job.params.output_files[output_src]:
         int(end_filenum)
     except:
         raise Exception("End file number '%s' has bad format." % end_filenum)
-    output_dest = output_basename + "_" + start_filenum + "-" + end_filenum + ".slcio"
-    job.params.output_files[output_src] = output_dest
+    output_src = output_basename + "_" + start_filenum + "-" + end_filenum + ".slcio"
+    job.params.output_files[output_src] = output_src
+else:
+    output_src = job.params.output_files.keys()[0]
     
 concat = LCIOConcat(inputs=input_files, outputs=[output_src])
 
 job.components = [concat]
-
 job.run()
