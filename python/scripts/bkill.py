64660642d3e8e993574dc611cb7e77e46f98e792
JeremyMcCormick <jermccormick@gmail.com>
2017-10-24 18:28:45 -0700

    Remove all batch wrapper scripts for now; they may be replaced with versions that use Ganga or that framework will be used directly.

--- python/scripts/bkill.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from hpsmc.batch import LSF
-
-if __name__ == "__main__":
-    lsf = LSF()
-    lsf.parse_args()
-    lsf.kill_all()
--- python/scripts/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from hpsmc.batch import Local
-
-if __name__ == "__main__":
-    b = Local()
-    b.parse_args()
-    b.submit_all()
