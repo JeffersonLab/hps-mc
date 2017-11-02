bdd40e30668503a80abf1226b58551f1ec17d073
JeremyMcCormick <jermccormick@gmail.com>
2017-11-02 15:24:43 -0700

    Example script that counts LCIO events.

---
+++ python/jobs/lcio_count_job.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+from hpsmc.job import Job
+from hpsmc.tools import LCIOCount
+
+job = Job(name="LCIO count job")
+job.initialize()
+
+output_files = sorted(job.params.output_files.keys())
+if len(output_files) < 1:
+    raise Exception("Not enough output files were provided (at least 1 required).")
+
+nevents = job.params.nevents
+
+count = LCIOCount(minevents=nevents, inputs=output_files)
+job.components = [count]
+job.enable_copy_output_files = False
+job.run()
