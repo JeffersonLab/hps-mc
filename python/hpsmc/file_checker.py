121d3850c5c60d66a8986c5a3eea736e9e769be4
JeremyMcCormick <jermccormick@gmail.com>
2017-11-02 18:07:57 -0700

    Add post processing hook to check for output files.

---
+++ python/hpsmc/file_checker.py
@@ -0,0 +1,16 @@
+import os, json
+
+def check(j):    
+    jsonfile = j.application.args[1]
+    with open(jsonfile) as datafile:
+        data = json.load(datafile)    
+    output_dir = ""
+    if "output_dir" in data:
+        output_dir = data["output_dir"]
+    if "output_files" in data:
+        outputfiles = data["output_files"]
+        for k,v in outputfiles.iteritems():
+            fpath = output_dir + os.path.sep + v
+            if not os.path.exists(fpath):
+                return False
+    return True
