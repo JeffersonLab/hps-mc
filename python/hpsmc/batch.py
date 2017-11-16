58e51b387be5f2a4e8eafdb9e2b61cc25338f8a5
JeremyMcCormick <jermccormick@gmail.com>
2017-11-15 16:04:23 -0800

    Add max jobs arg.

--- python/hpsmc/batch.py
+++ python/hpsmc/batch.py
@@ -6,16 +6,17 @@
 #                                               #
 #################################################
 
-import argparse, time
+import argparse, time, sys
 from ganga import *
 
-def subjobs(wf, nsub, waittime):
+def subjobs(wf, nsub, waittime, maxjobs):
 
     if not jobtree.exists("/" + wf):
         raise Exception("The workflow dir '/" + wf + "' does not exist!")
     jobtree.cd("/" + wf)
 
     subjobs = []
+    subcount = 0
     while True:
         jobsrunning = True
         runMonitoring()
@@ -36,16 +37,23 @@ def subjobs(wf, nsub, waittime):
                     print "Submitting job: " + str(j)
                     try:
                         jobs(j).submit()
+                        subcount += 1
+                        if subcount >= maxjobs:
+                            print "Reached max jobs of %d." % maxjobs
+                            break
                     except Exception as e:
                         print "Error during job submission: " + e.message
                         if jobs(j).status != 'new':
                             jobs(j).force_status('failed')
                 print "Submitted jobs: " + str(subjobs)
-            print "Sleeping for " + str(waittime) + " seconds ..."
-            time.sleep(waittime)
-            print "Done sleeping!"
+            if subcount < maxjobs:
+                print "Sleeping for " + str(waittime) + " seconds ..."
+                time.sleep(waittime)
+                print "Done sleeping!"
         else:
             print "No more jobs to submit."
+            break
+        if subcount >= maxjobs:
             break
 
 if __name__ == "__main__":
@@ -54,12 +62,14 @@ if __name__ == "__main__":
     parser.add_argument("-w", "--workflow", nargs="?", help="Name of the workflow (e.g. name of top-level job dir in Ganga)", required=True)
     parser.add_argument("-n", "--njobs", nargs="?", type=int, default=200, help="Number of jobs to submit in one batch")
     parser.add_argument("-t", "--time", nargs="?", type=int, default=60*5, help="Number of seconds to wait between batch submissions")
+    parser.add_argument("-m", "--max", nargs="?", type=int, default=sys.maxint, help="Maximum number of jobs to submit")
     cl = parser.parse_args()
 
     wf = cl.workflow
     nsub = cl.njobs
     waittime = cl.time
+    maxjobs = cl.max
 
     print "Submitting workflow <" + wf + "> with batch size " + str(nsub) + " and wait time " + str(waittime)
 
-    subjobs(wf, nsub, waittime)
+    subjobs(wf, nsub, waittime, maxjobs)
