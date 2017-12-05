118da4f8b858f0f125747294577dcc8767d4bae0
JeremyMcCormick <jermccormick@gmail.com>
2017-12-05 13:44:23 -0800

    Add optional beam parameter settings.

--- python/jobs/beam_job.py
+++ python/jobs/beam_job.py
@@ -16,16 +16,30 @@ from hpsmc.tools import StdHepTool
 job = Job(name="beam job")
 job.initialize()
 
+params = job.params
+
 if len(job.input_files) > 1:
     raise Exception("Too many input files (expected 1 but got %d)." % len(job.input_files))
 
 input_file = "beam.stdhep"
-base,ext = os.path.splitext(input_file) 
+base,ext = os.path.splitext(input_file)
 
 rot = StdHepTool(name="beam_coords",
                  inputs=[input_file],
                  outputs=[base+"_rot.stdhep"])
 
+"""
+Add optional beam parameters.
+"""
+if hasattr(params, "beam_sigma_x"):
+    rot.args.extend(["-x", str(params.beam_sigma_x)])
+if hasattr(params, "beam_sigma_y"):
+    rot.args.extend(["-y", str(params.beam_sigma_y)])
+if hasattr(params, "target_z"):
+    rot.args.extend(["-z", str(params.target_z)])
+if hasattr(params, "beam_rotation"):
+    rot.args.extend(["-r", str(params.beam_rotation)])
+            
 sample = StdHepTool(name="random_sample",
                     inputs=[base+"_rot.stdhep"],
                     outputs=[base+"_sampled"])
