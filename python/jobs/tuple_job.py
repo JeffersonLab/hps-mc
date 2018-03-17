54d43a04d41381542794b0e3e0c2c582e8f4b0a8
JeremyMcCormick <jermccormick@gmail.com>
2018-03-16 17:54:53 -0700

    Handle file naming when multiple LCIO files are provided.

--- python/jobs/tuple_job.py
+++ python/jobs/tuple_job.py
@@ -13,11 +13,13 @@ from hpsmc.tools import JobManager, TarFiles, MakeTree
 job = Job(name="Make tuples job")
 job.initialize()
 params = job.params
-input_files = job.params.input_files
+input_files = sorted(job.params.input_files.keys())
 output_files = job.params.output_files
 
-# Get base output name from input file key.
-output_base,ext = os.path.splitext(input_files.keys()[0])
+output_base,ext = os.path.splitext(input_files[0])
+if (len(input_files) > 1):
+    # Strip off extension from name if multiple file inputs.
+    output_base,ext = os.path.splitext(output_base)
 
 # Job parameters may optionally specify number of events to read from LCIO file.
 if hasattr(params, "nevents"):
@@ -29,7 +31,7 @@ else:
 make_tuples = JobManager(steering_resource=params.tuple_steering,
                    run=params.run,
                    detector=params.detector,
-                   inputs=input_files.keys(),
+                   inputs=input_files,
                    outputs=[output_base],
                    nevents=nevents)
 job.components.append(make_tuples)
