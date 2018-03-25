6cfa15bbfb8418e5d73121ef7e5342891924362a
JeremyMcCormick <jermccormick@gmail.com>
2018-03-25 00:15:56 -0700

    Concatenate files in slic job if running many to one.

--- python/jobs/slic_job.py
+++ python/jobs/slic_job.py
@@ -2,22 +2,24 @@
 Python script for generating 'wab-beam-tri' events.
 """
 
-import sys, os
+import os
 
-import hpsmc.func as func
 from hpsmc.job import Job
-from hpsmc.run_params import RunParameters
-from hpsmc.generators import MG5, StdHepConverter
-from hpsmc.tools import StdHepTool, SLIC, JobManager
+from hpsmc.tools import SLIC, LCIOConcat
 
 # job init
 job = Job(name="SLIC job")
 job.initialize()
 params = job.params
 
+if not len(input_files):
+    raise Exception("Input file list is empty!")
+
 # create a SLIC component for each input file
+output_files = []
 for i in params.input_files.keys():
     fname = os.path.splitext(i)[0] + ".slcio"
+    output_files.append(fname)
     
     # generate events in slic
     slic = SLIC(description="Run detector simulation using SLIC",
@@ -28,5 +30,11 @@ for i in params.input_files.keys():
                 ignore_returncode=True)
 
     job.components.append(slic)
+
+# concatenate LCIO files if a single output file was given in the JSON
+if len(input_files) > 1 and len(output_files) == 1:
+    concat = LCIOConcat(inputs=[output_files], 
+                        output=[os.path.splitext(params.output_files.keys()[0])[0]])
+    job.components.append(concat)
                         
 job.run()
