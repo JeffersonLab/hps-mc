e0fa545789199dcb4406d70eec2439f26bf9965b
JeremyMcCormick <jermccormick@gmail.com>
2018-03-26 15:53:55 -0700

    add input and output file checks

--- python/jobs/slic_job.py
+++ python/jobs/slic_job.py
@@ -12,19 +12,23 @@ job = Job(name="SLIC job")
 job.initialize()
 params = job.params
 
-if not len(input_files):
+if not len(params.input_files):
     raise Exception("Input file list is empty!")
+
+if (len(params.input_files) != len(params.output_files)) and len(params.output_files) != 1:
+    raise Exception("Input and output file lists are not the same length!")
 
 # create a SLIC component for each input file
 output_files = []
-for i in params.input_files.keys():
-    fname = os.path.splitext(i)[0] + ".slcio"
+for io in zip(sorted(params.input_files.keys()), sorted(params.output_files.keys())):
+    # each output file is named from the JSON list
+    fname = os.path.splitext(io[1])[0] + ".slcio"
     output_files.append(fname)
     
-    # generate events in slic
+    # generate events in slic using the input stdhep file
     slic = SLIC(description="Run detector simulation using SLIC",
                 detector=params['detector'],
-                inputs=[i],
+                inputs=[io[0]],
                 outputs=[fname],
                 nevents=params['nevents'],
                 ignore_returncode=True)
@@ -32,9 +36,9 @@ for i in params.input_files.keys():
     job.components.append(slic)
 
 # concatenate LCIO files if a single output file was given in the JSON
-if len(input_files) > 1 and len(output_files) == 1:
+if len(params.input_files) > 1 and len(params.output_files) == 1:
     concat = LCIOConcat(inputs=[output_files], 
-                        output=[os.path.splitext(params.output_files.keys()[0])[0]])
+                        outputs=[os.path.splitext(params.output_files.keys()[0])[0]])
     job.components.append(concat)
                         
 job.run()
