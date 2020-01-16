69f277f34625085deee78a54849ca7e61b6dd9d8
Stanislava Sevova <ssevova@stanford.edu>
2020-01-16 02:18:59 -0800

    integrating SIMP MC MG5 configuration

---
+++ generators/madgraph5/src/simp/bin/internal/ufomodel/coupling_orders.py
@@ -0,0 +1,28 @@
+# This file was automatically created by FeynRules 2.3.26
+# Mathematica version: 10.0 for Linux x86 (64-bit) (December 4, 2014)
+# Date: Thu 26 Jan 2017 09:31:57
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+DQED = CouplingOrder(name = 'DQED',
+                     expansion_order = 99,
+                     hierarchy = 1)
+
+DQND = CouplingOrder(name = 'DQND',
+                     expansion_order = 99,
+                     hierarchy = 1)
+
+QND = CouplingOrder(name = 'QND',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
