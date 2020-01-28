69be86f559f819c0bc20e942d20b5eaf53a0a767
Jeremy McCormick <jermccormick@gmail.com>
2020-01-27 22:51:18 -0600

    Move run params to inline dict.

--- python/hpsmc/run_params.py
+++ python/hpsmc/run_params.py
@@ -1,16 +1,51 @@
 import os, sys, json
 
+run_params = {
+    "aprime_mass": {
+        "1pt05": [15, 20, 30, 40, 50, 60, 70, 80, 90],
+        "1pt1": [15, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+        "1pt92": [50, 75, 100],
+        "2pt2": [15, 25, 50, 75, 100, 150, 200, 250],
+        "2pt3": [15, 25, 50, 75, 100, 150, 200, 250],
+        "4pt4": [15, 25, 50, 75, 100, 150, 200, 250, 300, 350, 400, 450, 500],
+        "6pt6": [50, 100, 200, 300, 400, 500, 600]
+    },
+    "target_z": {
+        "1pt1": 0.0004062,
+        "1pt92": 0.0004062,
+        "1pt05": 0.0004062,
+        "2pt2": 0.0004062,
+        "2pt3": 0.0004062,
+        "4pt4": 0.0004062,
+        "6pt6": 0.000875
+    },
+    "beam_energy": {
+        "1pt1": 1100.00,
+        "1pt92": 1920.00,
+        "1pt05": 1056.00,
+        "2pt2": 2200.00,
+        "2pt3": 2300.00,
+        "4pt4": 4400.00,
+        "6pt6": 6600.00
+    },
+    "num_electrons": {
+        "1pt05": 625,
+        "1pt1": 625,
+        "1pt92": 2500,
+        "2pt2": 2500,
+        "2pt3": 2500,
+        "4pt4": 5000,
+        "6pt6": 5625
+    }
+}
+
 class RunParameters:
 
     def __init__(self, key, json_filename = None):
         self.key = key
-        if json_filename is None:
-            json_filename = os.path.join(os.environ["HPSMC_DATA_DIR"], "run_params.json")
-        rawdata = open(json_filename, 'r').read()
-        self.json_dict = json.loads(rawdata)
-
+        
     def get(self, param_name):
-        return self.json_dict["run_params"][param_name][self.key]
+        return run_params[param_name][self.key]
 
 if __name__ == "__main__":
 
