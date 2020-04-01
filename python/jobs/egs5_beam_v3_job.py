284e8b4160f24e134c1611dcb481057367275f79
Jeremy McCormick <jermccormick@gmail.com>
2020-04-01 20:31:56 +1300

    Make EGS5 scripts up to date.

--- python/jobs/egs5_beam_v3_job.py
+++ python/jobs/egs5_beam_v3_job.py
@@ -4,16 +4,6 @@ from hpsmc.job import Job
 from hpsmc.generators import EGS5
 from hpsmc.run_params import RunParameters
 
-job = Job(name="EGS5 beam_v3 job")
-job.initialize()
-
-params = job.params
-
-egs5 = EGS5(name="beam_v3",
-    bunches=params.bunches,
-    run_params=RunParameters(key=params.run_params),
-    outputs=["beam.stdhep"])
-
-job.components = [egs5]
-
+job = Job()
+job.add(EGS5(name="beam_v3"))
 job.run()
