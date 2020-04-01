86be3875a3024ccde3d5c3aaa9da05e5ea9afade
Jeremy McCormick <jermccormick@gmail.com>
2020-04-01 22:37:43 +1300

    Simplify job script.

--- python/jobs/hps_java_job.py
+++ python/jobs/hps_java_job.py
@@ -7,6 +7,6 @@ import sys, random, os
 from hpsmc.job import Job
 from hpsmc.tools import JobManager
 
-job = Job(name="hps_java_job")
-job.add(JobManager(description="Run hps-java with a steering file"))
+job = Job()
+job.add(JobManager())
 job.run()
