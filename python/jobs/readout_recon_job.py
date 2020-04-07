2faef9531c97c57a7f951130373660e4d97132f2
Jeremy McCormick <jermccormick@gmail.com>
2020-04-07 12:06:58 -0700

    Add job script for running readout and recon in hps-java.

---
+++ python/jobs/readout_recon_job.py
@@ -0,0 +1,22 @@
+import sys, random
+
+from hpsmc.job import Job
+from hpsmc.run_params import RunParameters
+from hpsmc.generators import MG5, StdHepConverter
+from hpsmc.tools import SLIC, JobManager, FilterBunches, StdHepTool
+
+job = Job()
+
+# Insert empty bunches expected by pile-up simulation
+filter_bunches = FilterBunches(nevents=2000000, append='_filt')
+
+# Run simulated events in readout to generate triggers
+readout = JobManager(steering='readout',
+                    replace={'_filt': '_readout'})
+
+# Run physics reconstruction
+recon = JobManager(steering='recon',
+                   replace={'_readout': '_recon'})
+
+job.add([filter_bunches, readout, recon])
+job.run()
