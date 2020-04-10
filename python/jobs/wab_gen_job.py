c4b956139a99b9c6b9a936487f58de8ea2bd622d
Jeremy McCormick <jermccormick@gmail.com>
2020-04-10 13:14:56 -0700

    Minor changes and file renaming.

---
+++ python/jobs/wab_gen_job.py
@@ -0,0 +1,21 @@
+"""
+Python script for generating WAB events in MG4.
+"""
+
+from hpsmc.job import Job
+from hpsmc.run_params import RunParameters
+from hpsmc.generators import MG4, StdHepConverter
+from hpsmc.tools import LHECount
+
+job = Job()
+
+# generate tritrig in MG5
+mg = MG4(name="wab")
+
+# check that at least 80% of the requested events were generated or fail the job
+#check = LHECount(minevents=params.nevents*0.8, inputs=["wab_unweighted_events.lhe.gz"])
+
+# run the job
+#job.components=[mg, check]
+job.add([mg])
+job.run()
