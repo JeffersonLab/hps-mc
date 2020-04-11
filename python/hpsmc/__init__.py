f899d03caff4f9cc9fe47899880c9cd5fc389f07
Jeremy McCormick <jermccormick@gmail.com>
2020-04-10 21:52:40 -0700

    Turn on debug prints.

--- python/hpsmc/__init__.py
+++ python/hpsmc/__init__.py
@@ -2,7 +2,7 @@
 import logging, sys
 
 global_logger = logging.getLogger("hpsmc")
-global_logger.setLevel(logging.INFO)
+global_logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter('%(name)s:%(levelname)s %(message)s'))
 global_logger.addHandler(handler)
