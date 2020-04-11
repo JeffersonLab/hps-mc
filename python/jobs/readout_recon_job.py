de64c930bab36d1a515a356ba0058f4f2d817621
Jeremy McCormick <jermccormick@gmail.com>
2020-04-10 21:48:35 -0700

    Update readout & recon script.

--- python/jobs/readout_recon_job.py
+++ python/jobs/readout_recon_job.py
@@ -8,16 +8,13 @@ from hpsmc.tools import SLIC, JobManager, FilterBunches, StdHepTool
 job = Job()
 
 # Insert empty bunches expected by pile-up simulation
-filter_bunches = FilterBunches(nevents=2000000,
-                               append='_filt')
+filter_bunches = FilterBunches(nevents=2000000)
 
 # Run simulated events in readout to generate triggers
-readout = JobManager(steering='readout',
-                    replace={'_filt': '_readout'})
+readout = JobManager(steering='readout')
 
 # Run physics reconstruction
-reco = JobManager(steering='recon',
-                   replace={'_readout': '_recon'})
+reco = JobManager(steering='recon')
 
 job.add([filter_bunches, readout, reco])
 job.run()
