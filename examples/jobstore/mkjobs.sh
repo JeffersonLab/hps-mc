43d25fda9c402a5b685af27e6b3311db174223a8
Jeremy McCormick <jermccormick@gmail.com>
2020-04-17 02:21:40 -0500

    Add JSON job store for storing data for multiple jobs. Allow jobs to run from store based on job ID. Provide an example.

---
+++ examples/jobstore/mkjobs.sh
@@ -0,0 +1,2 @@
+# Creates a job store starting with job ID 100 from the input file list
+hps-mc-jobstore -j 100 -i filelist.txt -o events.slcio job.json.tmpl jobs.json
---
+++ examples/jobstore/run.sh
@@ -0,0 +1,1 @@
+hps-mc-job slic -i 100 -d $PWD/scratch -c .hpsmc jobs.json
