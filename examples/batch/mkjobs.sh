7b024d4b8e18a1157f13885849c929d20ed1017b
Jeremy McCormick <jermccormick@gmail.com>
2020-04-17 23:08:36 -0500

    Add support for generic variable substitution and iteration to job store.

--- examples/batch/mkjobs.sh
+++ examples/batch/mkjobs.sh
@@ -1,2 +1,3 @@
 # Creates a job store starting with job ID 100 from the input file list
-hps-mc-jobstore -j 100 -i filelist.txt -o events.slcio job.json.tmpl jobs.json
+hps-mc-jobstore -j 100 -i filelist.txt -a vars.json -o events.slcio job.json.tmpl jobs.json
+#hps-mc-jobstore -j 100 -i filelist.txt -o events.slcio job.json.tmpl jobs.json
--- python/hpsmc/job_store.py
+++ python/hpsmc/job_store.py
@@ -1,4 +1,4 @@
-import argparse, os, json, glob, collections, logging
+import argparse, os, json, glob, collections, logging, itertools
 from string import Template
 import hpsmc.util as util
 
@@ -6,10 +6,17 @@ logger = logging.getLogger("hpsmc.job_store")
 
 class JobStore:
     """
-    Create a JSON job store using a JSON template and list of files.
+    Create a JSON job store using a JSON template and variable substitutions. 
+    
+    The user may provide a file containing a list of input files to process as 
+    well as a JSON file containing variable lists. All combinations of the 
+    input files and variables are combined together using itertools and the 
+    template is processed to expand in a full set of jobs, which are written 
+    into a JSON job store. The input file list and the variable file are both
+    optional but at least one of them must be provided.
     """
         
-    def __init__(self, path=None):
+    def __init__(self, path=None):        
         self.path = path
         if path:
             logger.info("Initializing job store from '%s'" % self.path)
@@ -22,8 +29,9 @@ class JobStore:
         parser.add_argument("-j", "--job-start", nargs="?", type=int, help="Starting job ID", default=0)
         parser.add_argument("-p", "--pad", nargs="?", type=int, help="Number of padding spaces for job IDs (default is 4)", default=4)
         parser.add_argument("-r", "--seed", nargs="?", type=int, help="Starting random seed, incremented by 1 for each job", default=1)
-        parser.add_argument("-i", "--input-files", help="Input file list (text file)", required=True)
-        parser.add_argument("-o", "--output-file", help="Output file written by the job script", required=True)
+        parser.add_argument("-i", "--input-files", help="Input file list (text file)")
+        parser.add_argument("-o", "--output-file", help="Output file written by the job script")
+        parser.add_argument("-a", "--var-file", help="Variables in JSON format for iteration")
         parser.add_argument("json_template_file", help="Job template in JSON format")
         parser.add_argument("job_store", help="Output file containing the JSON job store")
         cl = parser.parse_args()
@@ -42,44 +50,84 @@ class JobStore:
         input_files = cl.input_files
         with open(input_files) as f:
             self.input_files = f.read().splitlines()
-            
-        self.output_file = cl.output_file
-            
+
+        if cl.output_file:           
+            self.output_file = cl.output_file
+        else:
+            self.output_file = None
+        
+        if cl.var_file:
+            var_file = cl.var_file
+            print("Iteration variables will be read from '%s'" % var_file)
+            if not os.path.exists(var_file):
+                raise Exception("The var file '%s' does not exist." % var_file)
+            with open(var_file, 'r') as f:
+                self.itervars = json.load(f)
+        else:
+            self.itervars = None
+ 
+    def get_iter_vars(self):
+        """
+        Return all combinations of the (optional) input files with the iteration variables.
+        """
+        var_list = []
+        var_names = []
+        if len(self.input_files):
+            var_names.append('input_file')
+            var_list.append(self.input_files)
+        if self.itervars:
+            var_names.extend(sorted(self.itervars.keys()))
+            for k in sorted(self.itervars.keys()):
+                var_list.append(self.itervars[k])
+        prod = itertools.product(*var_list)
+        return var_names, list(prod)
+                
     def build(self):
         """
-        Build a job store from the JSON template and input file list.
+        Build a job store from the JSON template and variable iterations.
         """ 
-               
+        
+        # Get the iteration variable names and value lists
+        var_names, var_lists = self.get_iter_vars()
+
+        # Setup the basic variable mapping for the template
         job_id = self.job_start
         mapping = {
             "job_id": None,
-            "input_file": None,
-            "output_file": self.output_file,
             "seed": None
         }
+        if self.output_file:
+            mapping["output_file"] = self.output_file
         seed = self.seed
+
+        # Dict which will contain all jobs
         jobs = {}
+        
+        # Read in the template file
         with open(self.json_template_file, 'r') as tmpl_file:
             tmpl = Template(tmpl_file.read())
-            
-        print("Processing %d input files ..." % len(self.input_files))
-        for input_file in self.input_files:
+        
+        # Loop over the variable lists and substitute into the template
+        for var_list in var_lists:
+            for varname, value in zip(var_names, var_list):
+                mapping[varname] = value                
+                #print('%s=%s' % (str(varname), str(value)))
             job_id_padded = ("%0" + str(self.job_id_pad) + "d") % job_id
             mapping['job_id'] = job_id_padded
-            mapping['input_file'] = input_file
             mapping['seed'] = seed
             job_str = tmpl.substitute(mapping)
             job_json = json.loads(job_str)
             jobs[str(job_id)] = job_json
             job_id +=1
-            seed +=1
-                
+            seed +=1 
+        
         self.data = jobs
         
+        # Dump the results to a file
         with open(self.job_store, 'w') as f:
             json.dump(jobs, f, indent=4)
                 
-        print("Wrote job store to '%s'" % self.job_store)
+        print("Wrote %d jobs to job store '%s'" % (len(self.data), self.job_store))
             
     def load(self, json_store):
         """Load raw JSON data into this job store."""
@@ -98,7 +146,11 @@ class JobStore:
         """Get a sorted list of job IDs."""
         return sorted(self.data.keys())
     
-# Run from the command line to create a job store
+    def has_job_id(self, job_id):
+        """Return true if the job ID exists in the store."""
+        return job_id in self.data.keys()
+    
+# Run from the command line to create a new job store
 if __name__ == "__main__":
     jobstore = JobStore()
     jobstore.parse_args()
