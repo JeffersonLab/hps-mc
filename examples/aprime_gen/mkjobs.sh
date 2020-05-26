9704d3bd7b217637e14e7707fa1eca6c77648855
Jeremy McCormick <jermccormick@gmail.com>
2020-05-26 17:00:33 -0500

    Updates to examples.

--- examples/aprime_gen/mkjobs.sh
+++ examples/aprime_gen/mkjobs.sh
@@ -1,1 +1,1 @@
-hps-mc-jobstore --repeat 10 --job-start 1 --var-file vars.json --output-file events.slcio --seed 1000000 job.json.tmpl jobs.json
+hps-mc-jobstore --repeat 5 --job-start 1 --var-file vars.json --seed 1000000 job.json.tmpl jobs.json
