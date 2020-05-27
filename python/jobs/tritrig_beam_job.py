e5278296cc910f499d60d05c59a9b4f042002a32
Jeremy McCormick <jermccormick@gmail.com>
2020-05-27 13:01:25 -0500

    Updates to tritrig-beam script (not in its final form yet).

--- python/jobs/tritrig_beam_job.py
+++ python/jobs/tritrig_beam_job.py
@@ -1,21 +1,31 @@
 from hpsmc.generators import StdHepConverter
 from hpsmc.tools import BeamCoords, AddMother, MergePoisson, RandomSample, MergeFiles
-from hpsmc.tools import SLIC, JobManager, FilterBunches, HPSTR, LCIOCount
+from hpsmc.tools import SLIC, JobManager, FilterBunches, HPSTR, LCIOCount, LCIOMerge, StdHepCount
 
 # Get job input file targets
 inputs = job.input_files.values()
+
+if 'event_interval' in job.params:
+    event_interval = job.params['event_interval']
+else:
+    event_interval = 250
+
+if 'nevents' in job.params:
+    nevents = job.params['nevents']
+else:
+    nevents = 8000
 
 # Input tritrig events (LHE format)
 tritrig_file_name = 'tritrig_events.lhe.gz'
 
 # Input beam events (StdHep format)
-#beam_file_name = 'beam.stdhep'
+beam_file_name = 'beam.stdhep'
 
 # Check for expected input file targets
 if tritrig_file_name not in inputs:
     raise Exception("Missing required input file '%s'" % tritrig_file_name)
-#if beam_file_name not in inputs:
-#    raise Exception("Missing required input file '%s'" % beam_file_name)
+if beam_file_name not in inputs:
+    raise Exception("Missing required input file '%s'" % beam_file_name)
 
 # Base name of intermediate tritrig files
 tritrig_name = 'tritrig'
@@ -38,43 +48,52 @@ mom = AddMother(inputs=cnv.output_files(),
 rot = BeamCoords(inputs=mom.output_files(),
                 outputs=['%s_rot.stdhep' % tritrig_name])
 
-# Sample tritrig events using poisson distribution
-#sample = MergePoisson(lhe_file=tritrig_file_name, # for calculating mu
-#                      inputs=rot.output_files(),
-#                      outputs=['%s_sampled.stdhep' % tritrig_name],
-#                      nevents=500000)
+# Simulate signal events
+slic = SLIC(inputs=rot.output_files(),
+            outputs=['%s.slcio' % tritrig_name])
+
+# Space signal events before merging
+filter_bunches = FilterBunches(nevents=nevents*event_interval,
+                               inputs=slic.output_files(),
+                               outputs=['%s_filt.slcio' % tritrig_name])
+
+# Count filtered events
+count_filter = LCIOCount(inputs=filter_bunches.output_files())
 
 # Transform beam events
-#rot_beam = BeamCoords(inputs=[beam_file_name],
-#                      outputs=['%s_rot.stdhep' % beam_name])
+rot_beam = BeamCoords(inputs=[beam_file_name],
+                      outputs=['%s_rot.stdhep' % beam_name])
 
 # Sample the beam events
 #sample_beam = RandomSample(inputs=rot_beam.output_files(),
-#                           outputs=['%s_sampled.stdhep' % beam_name])
+#                           outputs=['%s_sampled.stdhep' % beam_name],
+#                           nevents=nevents*event_interval)
 
-# Merge signal and background events
-#merge = MergeFiles(inputs=[sample.output_files()[0],
-#                           sample_beam.output_files()[0]],
-#                   outputs=['%s.stdhep' % tritrig_beam_name])
+# Count sampled beam events
+count_beam = StdHepCount(inputs=rot_beam.output_files())
 
-# Run simulation
-#slic = SLIC(inputs=merge.output_files(),
-#            outputs=['%s.slcio' % tritrig_beam_name])
-slic = SLIC(inputs=rot.output_files(),
-            outputs=['%s.slcio' % tritrig_beam_name])
+# Simulate beam events
+#_sampled
+slic_beam = SLIC(inputs=rot_beam.output_files(),
+                 outputs=['%s.slcio' % beam_name],
+                 nevents=nevents*event_interval,
+                 ignore_job_params=['nevents'])
 
-# Space events for readout simulation
-filter_bunches = FilterBunches(nevents=2000000,
-                               inputs=slic.output_files(),
-                               outputs=['%s_filt.slcio' % tritrig_beam_name])
+# Merge signal and beam events
+merge = LCIOMerge(inputs=[slic.output_files()[0], 
+                          slic_beam.output_files()[0]],
+                  outputs=['%s.slcio' % tritrig_beam_name])
+
+# Count merged events
+count_merge = LCIOCount(inputs=merge.output_files())
 
 # Run simulated events in readout to generate triggers
-readout = JobManager(steering='readout', 
-                     inputs=filter_bunches.output_files(),
+readout = JobManager(steering='readout',
+                     inputs=merge.output_files(),
                      outputs=['%s_readout.slcio' % tritrig_beam_name])
 
 # Print number of readout events
-#count_readout = LCIOCount(inputs=readout.output_files())
+count_readout = LCIOCount(inputs=readout.output_files())
 
 # Run physics reconstruction
 recon = JobManager(steering='recon',
@@ -82,7 +101,7 @@ recon = JobManager(steering='recon',
                    outputs=['%s_recon.slcio' % tritrig_beam_name])
 
 # Print number of recon events
-#count_recon = LCIOCount(inputs=recon.output_files())
+count_recon = LCIOCount(inputs=recon.output_files())
 
 # Convert LCIO to ROOT
 #tuple = HPSTR(cfg='recon', 
@@ -95,8 +114,21 @@ recon = JobManager(steering='recon',
 #            outputs=['%s_ana.root' % tritrig_beam_name])
  
 # Add the components
-#job.add([cnv, mom, rot, sample, rot_beam, sample_beam, merge, slic,
-#         filter_bunches, readout, count_readout, recon, count_recon, 
-#         tuple, ana])
+#sample_beam, 
+job.add([cnv, mom, rot, slic, filter_bunches, count_filter, rot_beam, 
+         count_beam, slic_beam, merge, count_merge, readout, count_readout, 
+         recon])
 
-job.add([cnv, mom, rot, slic, filter_bunches, readout, recon])
+job.add([ ])
+
+
+# Sample tritrig events using poisson distribution
+#sample = MergePoisson(lhe_file=tritrig_file_name, # for calculating mu
+#                      inputs=rot.output_files(),
+#                      outputs=['%s_sampled.stdhep' % tritrig_name],
+#                      nevents=500000)
+
+# Merge signal and background events
+#merge = MergeFiles(inputs=[sample.output_files()[0],
+#                           sample_beam.output_files()[0]],
+#                   outputs=['%s.stdhep' % tritrig_beam_name])
