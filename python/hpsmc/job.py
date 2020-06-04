74c1fe1cda154243d716b5915f5cb04b0af95927
HPS (baltzell) <hps@ifarm1802.jlab.org>
2020-06-04 19:27:22 -0400

    fixing for job.py

--- python/hpsmc/job.py
+++ python/hpsmc/job.py
@@ -396,7 +396,8 @@ class Job(object):
             if i == 0:
                 logger.info("Setting inputs on '%s' to: %s"
                             % (c.name, str(self.input_files.values())))
-                c.inputs = self.input_files.values()
+                if not len(c.inputs):
+			c.inputs = self.input_files.values()
             elif i > -1:
                 logger.info("Setting inputs on '%s' to: %s"
                             % (c.name, str(self.components[i - 1].output_files())))
