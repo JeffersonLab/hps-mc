516ba8c2658e765fb54758021003c6bb14abcc3b
Jeremy McCormick <jermccormick@gmail.com>
2020-06-07 02:21:02 -0500

    Show how to run slic with log files or writing all output to console.

--- examples/slic/run.sh
+++ examples/slic/run.sh
@@ -1,1 +1,8 @@
-hps-mc-job run -n -d $PWD/scratch -c .hpsmc slic job.json
+# Save output to three separate files (stdout and stderr of components and logging output)
+hps-mc-job run -n -d $PWD/scratch -c .hpsmc -l job.log -o job.out -e job.err slic job.json
+
+# Print all output to the console
+#hps-mc-job run -n -d $PWD/scratch -c .hpsmc slic job.json
+
+# Don't do this! Same file name should NOT be used for stdout/stderr/logging.
+#hps-mc-job run -n -d $PWD/scratch -c .hpsmc -l job.log -o job.log -e job.log slic job.json
