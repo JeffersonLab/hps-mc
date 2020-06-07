47f2936280522b3e59fb4581b66471bbd28ec904
Jeremy McCormick <jermccormick@gmail.com>
2020-06-07 02:32:16 -0500

    Include example job ID padding.

--- examples/batch/mkjobs.sh
+++ examples/batch/mkjobs.sh
@@ -1,1 +1,2 @@
-hps-mc-jobstore -r 8 -j 1 -i events.txt 1 -a vars.json job.json.tmpl jobs.json
+# Create 8 jobs starting from job ID 1 with padding of 4 chars, reading 1 event per job 
+hps-mc-jobstore -r 8 -j 1 -p 4 -i events.txt 1 -a vars.json job.json.tmpl jobs.json
