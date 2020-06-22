5681fe830447ec68abfbd8fdb39b62b4602d220a
Jeremy McCormick <jermccormick@gmail.com>
2020-06-22 15:51:12 -0500

    Remove lcio count so that the jar is not needed for job.

--- python/jobs/readout_recon_job.py
+++ python/jobs/readout_recon_job.py
@@ -5,14 +5,10 @@ job.description = 'Filter bunches with readout and recon'
 # Insert empty bunches expected by pile-up simulation
 filter_bunches = FilterBunches(nevents=2000000)
 
-count_filter = LCIOCount()
-
 # Run simulated events in readout to generate triggers
 readout = JobManager(steering='readout')
 
 # Run physics reconstruction
 reco = JobManager(steering='recon')
 
-#job.ptag('recon', reco.output_files()[0])
-
-job.add([filter_bunches, count_filter, readout, reco])
+job.add([filter_bunches, readout, reco])
