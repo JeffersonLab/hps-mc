7ddb1bdff5d34b70bbd1fd984ec277e6c0e37631
Tongtong Cao <caot@ifarm1901.jlab.org>
2020-06-30 21:43:03 -0400

    update of example tritrig_beam_merge_to_recon

--- examples/tritrig_beam_merge_to_recon/run_auger.sh
+++ examples/tritrig_beam_merge_to_recon/run_auger.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-hps-mc-batch auger -D -c .hpsmc -W 4 -l $PWD/logs tritrig_beam_merge_to_recon jobs.json
+hps-mc-batch auger -D -c ../../config/jlab_tongtong.cfg -c .hpsmc -W 4 -l $PWD/logs signal_beam_merge_to_recon jobs.json
