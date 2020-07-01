14c1087a8334d08e45b2f2bb4fc46f0a2df18249
Tongtong Cao <caot@ifarm1901.jlab.org>
2020-06-30 20:50:07 -0400

    update for example tritrig_gen_to_slic

--- examples/tritrig_gen_to_slic/run_auger.sh
+++ examples/tritrig_gen_to_slic/run_auger.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-hps-mc-batch auger -D -c .hpsmc -W 4 -l $PWD/logs tritrig_gen_to_slic jobs.json
+hps-mc-batch auger -D -c ../../config/jlab_tongtong.cfg -c .hpsmc -W 4 -l $PWD/logs tritrig_gen_to_slic jobs.json
