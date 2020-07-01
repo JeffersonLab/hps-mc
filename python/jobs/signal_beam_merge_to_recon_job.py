663d0d0382e1d10929e55e1c5f1b9379ececec0c
Tongtong Cao <caot@ifarm1901.jlab.org>
2020-06-30 21:41:29 -0400

    update job script tritrig_beam_merge_to_recon_job.py as signal_beam_merge_to_recon_job.py for aplication of all signal-beam jobs

--- python/jobs/signal_beam_merge_to_recon_job.py
+++ python/jobs/signal_beam_merge_to_recon_job.py
@@ -1,40 +1,40 @@
 from hpsmc.tools import ExtractEventsWithHitAtHodoEcal, JobManager, LCIOCount, LCIOMerge
 
-job.description = 'tritrig-beam from merge to recon'
+job.description = 'signal-beam from merge to recon'
 
 # Get job input file targets
 inputs = job.input_files.values()
 
-# Input tritrig events (slcio format)
-tritrig_file_name = [] 
+# Input signal events (slcio format)
+signal_file_name = [] 
 
 # Input beam events (slcio format)
 beam_file_name = [] 
 
 for input in inputs:
-    if "tritrig" in input:
-        tritrig_file_name.append(input)
+    if "signal" in input:
+        signal_file_name.append(input)
     if "beam" in input:
         beam_file_name.append(input)
 
 # Check for expected input file targets
-if len(tritrig_file_name) == 0:
-    raise Exception("Missing required input file(s) for tritrig")
+if len(signal_file_name) == 0:
+    raise Exception("Missing required input file(s) for signal")
 if len(beam_file_name) == 0:
     raise Exception("Missing required input file(s) for beam")
 
-# Base name of intermediate tritrig files
-tritrig_name = 'tritrig'
+# Base name of intermediate signal files
+signal_name = 'signal'
 
 # Base name of intermediate beam files
 beam_name = 'beam'
 
 # Base name of merged files
-tritrig_beam_name = 'tritrig-beam'
+signal_beam_name = 'signal-beam'
 
 # Filter and space signal events and catenate files before merging
-filter_events = ExtractEventsWithHitAtHodoEcal(inputs=tritrig_file_name,
-                                               outputs=['%s_filt.slcio' % tritrig_name], 
+filter_events = ExtractEventsWithHitAtHodoEcal(inputs=signal_file_name,
+                                               outputs=['%s_filt.slcio' % signal_name], 
                                                event_interval=250, num_hodo_hits=1)
 
 # Count filtered events
@@ -51,7 +51,7 @@ count_beam = LCIOCount(inputs=catenate_beam.output_files())
 # Merge signal and beam events
 merge = LCIOMerge(inputs=[filter_events.output_files()[0],
                           catenate_beam.output_files()[0]],
-                  outputs=['%s.slcio' % tritrig_beam_name],
+                  outputs=['%s.slcio' % signal_beam_name],
                   ignore_job_params=['nevents'])
 
 # Print number of merged events
@@ -60,7 +60,7 @@ count_merge = LCIOCount(inputs=merge.output_files())
 # Run simulated events in readout to generate triggers
 readout = JobManager(steering='readout',
                      inputs=merge.output_files(),
-                     outputs=['%s_readout.slcio' % tritrig_beam_name])
+                     outputs=['%s_readout.slcio' % signal_beam_name])
 
 # Print number of readout events
 count_readout = LCIOCount(inputs=readout.output_files())
@@ -68,7 +68,7 @@ count_readout = LCIOCount(inputs=readout.output_files())
 # Run physics reconstruction
 recon = JobManager(steering='recon',
                    inputs=readout.output_files(),
-                   outputs=['%s_recon.slcio' % tritrig_beam_name])
+                   outputs=['%s_recon.slcio' % signal_beam_name])
 
 # Print number of recon events
 count_recon = LCIOCount(inputs=recon.output_files())
