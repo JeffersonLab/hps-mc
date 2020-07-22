38e9aa211347182d8153841f640ae0576d46049f
Jeremy McCormick <jermccormick@gmail.com>
2020-07-22 00:54:41 -0400

    Count the number of events in all files.

--- python/jobs/readout_recon_job.py
+++ python/jobs/readout_recon_job.py
@@ -2,13 +2,21 @@ from hpsmc.tools import JobManager, FilterBunches, LCIOCount
 
 job.description = 'Filter bunches with readout and recon'
 
+count_input = LCIOCount()
+
 # Insert empty bunches expected by pile-up simulation
 filter_bunches = FilterBunches()
+
+count_filt = LCIOCount()
 
 # Run simulated events in readout to generate triggers
 readout = JobManager(steering='readout')
 
+count_readout = LCIOCount()
+
 # Run physics reconstruction
 reco = JobManager(steering='recon')
 
-job.add([filter_bunches, readout, reco])
+count_reco = LCIOCount()
+
+job.add([count_input, filter_bunches, count_filt, readout, count_readout, reco, count_reco])
