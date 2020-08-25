18d6244f814e109b937cb9964437a349af445b04
Jeremy McCormick <jermccormick@gmail.com>
2020-08-25 15:51:13 -0500

    Add stdhep cnv to tritrig gen script.

--- python/jobs/tritrig_gen_job.py
+++ python/jobs/tritrig_gen_job.py
@@ -1,9 +1,12 @@
-from hpsmc.generators import MG5
+from hpsmc.generators import MG5, StdHepConverter
 
-job.description = 'tritrig generation using MadGraph5'
+job.description = 'Generate tritrig events using MadGraph5 and convert to StdHep using EGS5'
 
 # Generate tritrig in MG5
 mg = MG5(name='tritrig')
 
+# Convert LHE output to stdhep
+cnv = StdHepConverter()
+
 # run the job
-job.add([mg])
+job.add([mg, cnv])
