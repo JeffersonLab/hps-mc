4a46ed833e38fb1f7b0cc565bf39c401ede4fd2c
Jeremy McCormick <jermccormick@gmail.com>
2020-09-26 19:30:33 -0500

    Update help so it is more helpful.

--- python/hpsmc/help.py
+++ python/hpsmc/help.py
@@ -1,5 +1,9 @@
 """Utility script for printing help about component classes."""
 
+from component import Component
+from job import Job
+
+# These are here so all available components appear in the globals dict.
 from tools import *
 from generators import *
 
@@ -45,11 +49,38 @@ def print_component(v):
 
 def print_components():
     """Print info for all Component classes."""
+    print("AVAILABLE COMPONENTS: ")
     for k in sorted(globals().keys()):
         v = globals()[k]
         if isinstance(v, Component.__class__):
             if v.__name__ not in _ignore:
-                print_component(v)
+                print('    {}'.format(v.__name__))
+
+def print_job_script(script_path):
+
+    job = Job()
+    job.script = script_path
+
+    # Dummy parameters so certain scripts don't crash when loaded.
+    job.input_files = {'signal': 'signal',
+                       'beam': 'beam'}
+    job.output_files = {'dummy.out': 'dummy.out'}
+    job.params['run_params'] = '4pt55'
+    job.params['nevents'] = 9999
+
+    job._load_script()
+
+    print('JOB SCRIPT: {}'.format(job.script))
+    print('')
+    print('    DESCRIPTION: {}'.format(job.description))
+    print('')
+    print('    COMPONENTS:')
+    for component in job.components:
+        print('        {}'.format(component.__class__.__name__))
+    print('')
+    print('    PTAGS:')
+    for ptag in job.ptags:
+        print('        {}'.format(ptag))
 
 if __name__ == '__main__':
     print_components()
