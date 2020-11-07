3e62de15e6af0edacd7d3682d26981bf9abf0d28
Jeremy McCormick <jermccormick@gmail.com>
2020-11-06 22:09:31 -0600

    Install hps-java detector data into share dir.

---
+++ cmake/InstallDetectorData.cmake
@@ -0,0 +1,24 @@
+set(DETECTOR_DATA "DetectorData")
+
+externalproject_add(
+    ${DETECTOR_DATA}
+    INSTALL_DIR       "${CMAKE_INSTALL_PREFIX}/share"
+    GIT_REPOSITORY    "https://github.com/JeffersonLab/hps-java"
+    GIT_SHALLOW       ON
+    SOURCE_DIR        ${CMAKE_BINARY_DIR}/${DETECTOR_DATA}
+    BUILD_COMMAND     ""
+    UPDATE_COMMAND    git pull
+    INSTALL_COMMAND   ""
+    CONFIGURE_COMMAND ""
+)
+
+add_dependencies(external ${DETECTOR_DATA})
+
+externalproject_get_property(${DETECTOR_DATA} SOURCE_DIR)
+externalproject_get_property(${DETECTOR_DATA} INSTALL_DIR)
+
+install(DIRECTORY ${SOURCE_DIR}/detector-data/detectors DESTINATION ${INSTALL_DIR}
+        FILES_MATCHING PATTERN "*.lcdd"
+        PATTERN "SamplingFractions" EXCLUDE)
+
+message(STATUS "${DETECTOR_DATA} will be installed to: ${INSTALL_DIR}")
