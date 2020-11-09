78abcaa9567965483126f4357bbae4d3435e9eac
Jeremy McCormick <jermccormick@gmail.com>
2020-11-09 15:59:43 -0600

    Cleanup cmake scripts.

---
+++ cmake/InstallConditions.cmake
@@ -0,0 +1,25 @@
+set(CONDITIONS              "Conditions")
+set(CONDITIONS_BUILD_DIR    ${CMAKE_BINARY_DIR}/${CONDITIONS})
+set(CONDITIONS_VERSION      2020_11_06)
+set(CONDITIONS_FILE         hps_conditions_${CONDITIONS_VERSION}.db)
+set(CONDITIONS_INSTALL_DIR  ${CMAKE_INSTALL_PREFIX}/share/conditions)
+
+externalproject_add(
+
+    ${CONDITIONS}
+
+    GIT_REPOSITORY    "git@github.com:JeffersonLab/hps-conditions-backup.git"
+    GIT_SHALLOW       ON
+
+    INSTALL_DIR       ${CONDITIONS_INSTALL_DIR}
+    SOURCE_DIR        ${CONDITIONS_BUILD_DIR}
+    BUILD_IN_SOURCE   ON
+
+    BUILD_COMMAND     cd ${CONDITIONS_BUILD_DIR} && tar -zxvf ${CONDITIONS_FILE}.tar.gz
+    INSTALL_COMMAND   cp ${CONDITIONS_FILE} ${CONDITIONS_INSTALL_DIR}
+    CONFIGURE_COMMAND ""
+)
+
+add_dependencies(external ${CONDITIONS})
+externalproject_get_property(${CONDITIONS} INSTALL_DIR)
+message(STATUS "${CONDITIONS} will be installed to: ${INSTALL_DIR}")
--- cmake/InstallHpsJava.cmake
+++ cmake/InstallHpsJava.cmake
@@ -7,20 +7,21 @@ set(HPSJAVA_BUILD_DIR    ${CMAKE_BINARY_DIR}/${HPSJAVA})
 set(HPSJAVA_BIN_JAR      ${HPSJAVA_INSTALL_DIR}/lib/${HPSJAVA_JAR_NAME})
 
 externalproject_add(
+
     ${HPSJAVA}
+
     INSTALL_DIR       ${CMAKE_INSTALL_PREFIX}
+    SOURCE_DIR        ${HPSJAVA_BUILD_DIR}
+
     GIT_REPOSITORY    "https://github.com/JeffersonLab/hps-java"
     GIT_TAG           ${HPSJAVA_TAG}
     GIT_SHALLOW       ON
-    SOURCE_DIR        ${HPSJAVA_BUILD_DIR}
+
     BUILD_COMMAND     cd ${HPSJAVA_BUILD_DIR} && ${MAVEN_BUILD_COMMAND}
     INSTALL_COMMAND   cp ${HPSJAVA_BUILD_DIR}/distribution/target/${HPSJAVA_JAR_NAME} ${HPSJAVA_BIN_JAR}
     CONFIGURE_COMMAND ""
 )
 
 add_dependencies(external ${HPSJAVA})
-
-#externalproject_get_property(${HPSJAVA} SOURCE_DIR)
-#externalproject_get_property(${HPSJAVA} INSTALL_DIR)
 
 message(STATUS "${HPSJAVA} bin jar will be installed to: ${HPSJAVA_BIN_JAR}")
