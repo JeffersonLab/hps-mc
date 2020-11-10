b65a1058af6e68ea7cb1cf0bcea1b90ea50ab892
Jeremy McCormick <jermccormick@gmail.com>
2020-11-09 19:05:35 -0600

    Minor changes to cmake external instal scripts.

--- cmake/InstallDetectorData.cmake
+++ cmake/InstallDetectorData.cmake
@@ -1,5 +1,8 @@
 set(DETECTOR_DATA "DetectorData")
 
+set(DETECTOR_DATA_SOURCE_DIR ${CMAKE_BINARY_DIR}/${DETECTOR_DATA})
+set(DETECTOR_DATA_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share)
+
 externalproject_add(
 
     ${DETECTOR_DATA}
@@ -7,12 +10,12 @@ externalproject_add(
     GIT_REPOSITORY    "https://github.com/JeffersonLab/hps-java"
     GIT_SHALLOW       ON
 
-    INSTALL_DIR       ${CMAKE_INSTALL_PREFIX}/share
-    SOURCE_DIR        ${CMAKE_BINARY_DIR}/${DETECTOR_DATA}
+    INSTALL_DIR       ${DETECTOR_DATA_INSTALL_DIR}
+    SOURCE_DIR        ${DETECTOR_DATA_SOURCE_DIR}
 
+    CONFIGURE_COMMAND ""
     BUILD_COMMAND     ""
     INSTALL_COMMAND   ""
-    CONFIGURE_COMMAND ""
 )
 
 add_dependencies(external ${DETECTOR_DATA})
@@ -20,7 +23,7 @@ add_dependencies(external ${DETECTOR_DATA})
 externalproject_get_property(${DETECTOR_DATA} SOURCE_DIR)
 externalproject_get_property(${DETECTOR_DATA} INSTALL_DIR)
 
-install(DIRECTORY ${SOURCE_DIR}/detectors DESTINATION ${INSTALL_DIR}
+install(DIRECTORY ${SOURCE_DIR}/detector-data/detectors DESTINATION ${INSTALL_DIR}
         FILES_MATCHING PATTERN "*.lcdd"
         PATTERN "SamplingFractions" EXCLUDE)
 
