c82a1cab917ce8cc36e1f233ee3913324ac69ade
Jeremy McCormick <jermccormick@gmail.com>
2020-11-10 22:14:03 -0600

    Add new classes for representing config and parameters in components.

---
+++ python/hpsmc/config.py
@@ -0,0 +1,165 @@
+import sys
+import os
+import configparser
+
+class ConfigItem:
+
+    def __init__(self,
+                 name,
+                 env_name=None,
+                 description='',
+                 optional=True,
+                 default=None,
+                 read_from_env=False,
+                 read_from_config=True):
+        self.name = name
+        if env_name is None:
+            self.env_name = self.name.upper()
+        else:
+            self.env_name = env_name
+        self.description = description
+        self.optional = optional
+        self.default = default
+        self.value = None
+        self.read_from_env = read_from_env
+        self.read_from_config = read_from_config
+
+    def set_from_default(self):
+        #print('Setting value from default: {} = {}'.format(self.name, self.default))
+        self.value = self.default
+
+    def set_from_env(self):
+        if os.environ.get(self.env_name) is not None and self.read_from_env:
+            self.value = os.environ.get(self.env_name, None)
+            #print('Set value from env: {} = {}'.format(self.env_name, self.value))
+
+    def is_required(self):
+        return not self.optional
+
+    def is_set(self):
+        return self.value is not None
+
+    def invalid(self):
+        return self.is_required() and not self.is_set()
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'env_name': self.env_name,
+            'description': self.description,
+            'optional': self.optional,
+            'default': self.default,
+            'value': self.value,
+            'read_from_env': self.read_from_env,
+            'read_from_config': self.read_from_config}
+
+    def __str__(self):
+        return str(self.to_dict())
+
+class Config:
+
+    def __init__(self, section, items = {}):
+        self.section = section
+        self.items = {}
+        if len(items):
+            self.add_items(items)
+
+    def add_items(self, items):
+        for config_item in items:
+            self.add_item(config_item)
+
+    def add_item(self, config_item):
+        if self.has_item(config_item.name):
+            raise Exception('Config item already exists: {}:{}'.format(self.section, config_item.name));
+        self.items[config_item.name] = config_item
+
+    def get_item(self, name):
+        if not self.has_item(name):
+            raise Exception('Config item does not exist: {}'.format(name))
+        return self.items[name]
+
+    def has_item(self, name):
+        return name in self.items.keys()
+
+    def load_defaults(self):
+        for item in self.items.values():
+            item.set_from_default()
+
+    def load_from_env(self):
+        for item in self.items.values():
+            item.set_from_env()
+
+    def load(self, parser):
+        """
+        Load from reference to configparser object.
+        """
+        if parser.has_section(self.section):
+            print('Found section: {}'.format(self.section))
+            for name,value in parser.items(self.section):
+                print('Processing item: {} = {}'.format(name, value))
+                if self.has_item(name):
+                    c = self.get_item(name)
+                    if c.read_from_config:
+                        c.value = value
+                        print('Set value from config: {} = {}'.format(c.name, value))
+
+    def get_invalid_items(self):
+        invalid_items = []
+        for item in self.items.values():
+            if item.invalid():
+                invalid_items.append(item)
+        return invalid_items
+
+    def validate(self):
+        return len(self.get_invalid_items()) == 0
+
+    def __str__(self):
+        return str([p.to_dict() for p in self.items.values()])
+
+class DummyConfigObject:
+
+    def __init__(self):
+        self.config = Config('Dummy',
+                             items=[ConfigItem('something'),
+                                    ConfigItem('another', optional=False),
+                                    ConfigItem('mojo', optional=False, default='bad'),
+                                    ConfigItem('vibes', optional=False, read_from_env=True)
+                                    ])
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        raise Exception('not enough args')
+
+    config_file = sys.argv[1]
+
+    parser = configparser.ConfigParser()
+    parsed = parser.read([config_file])
+
+    obj = DummyConfigObject()
+    obj_config = obj.config
+
+    valid = obj_config.validate()
+    print('valid (initial): {}'.format(valid))
+    for item in obj_config.get_invalid_items():
+        print(item)
+
+    obj_config.load_defaults()
+    valid = obj_config.validate()
+    print('valid (after defaults): {}'.format(valid))
+    for item in obj_config.get_invalid_items():
+        print(item)
+
+    obj_config.load(parser)
+    print('valid (after load): {}'.format(valid))
+    for item in obj_config.get_invalid_items():
+        print(item)
+
+    os.environ.__setitem__('VIBES', 'yes')
+    obj_config.load_from_env()
+    valid = obj_config.validate()
+    print('valid (after env): {}'.format(valid))
+    for item in obj_config.get_invalid_items():
+        print(item)
+
+    print(obj_config)
---
+++ python/hpsmc/parameters.py
@@ -0,0 +1,192 @@
+class Parameter(object):
+
+    """
+    Class for holding information about a component parameter
+    """
+
+    def __init__(self,
+                 name,
+                 description='',
+                 optional=True,
+                 default_value=None,
+                 read_from_dict=True,
+                 read_from_args=True):
+        self.name = name
+        self.description = description
+        self.optional = optional
+        self.default_value = default_value
+        self.value = None
+
+        self.read_from_dict = read_from_dict
+        self.read_from_args = read_from_args
+
+    def get_type(self):
+        return type('')
+
+    def str_value(self):
+        return str(self.value)
+
+    def set_value_from_default(self):
+        if self.default_value is not None:
+            self.value = self.default_value
+            print('Set parameter from default: {} = {}'.format(self.name, self.value))
+
+    def set_value(self, value):
+        self.value = self.convert_value(value)
+
+    def is_required(self):
+        return not self.optional
+
+    def is_default(self):
+        return self.value == self.default_value
+
+    def convert_value(self, value):
+        return value
+
+    def to_dict(self):
+        return {'name': self.name,
+                'description': self.description,
+                'optional': self.optional,
+                'default_value': self.default_value,
+                'value': self.value,
+                'type': self.get_type(),
+                'read_from_dict': self.read_from_dict,
+                'read_from_args': self.read_from_args
+            }
+
+    def __str__(self):
+        return str(self.to_dict())
+
+    def is_set(self):
+        return self.value is not None
+
+    def invalid(self):
+        return self.value is None and self.optional is False
+
+class FloatParameter(Parameter):
+
+    def get_type(self):
+        return type(1.0)
+
+    def convert_value(self, value):
+        return float(value)
+
+class IntParameter(Parameter):
+
+    def get_type(self):
+        return type(1)
+
+    def convert_value(self, value):
+        return int(value)
+
+class BoolParameter(Parameter):
+
+    def get_type(self):
+        return type(True)
+
+    def convert_value(self, value):
+        if value in [0, False, 'false', 'False', 'no', 'No']:
+            return False
+        else:
+            return True
+
+class ListParameter(Parameter):
+
+    def get_type(self):
+        return type([])
+
+    def convert_value(self, value):
+        return list(value)
+
+class ParameterSet(object):
+
+    def __init__(self, *args):
+        self.parameters = {}
+        self.add(*args)
+
+    def add(self, *args):
+        for param in args:
+            if self.has(param.name):
+                raise Exception("Parameter already exists: {}".format(param.name))
+            self.parameters[param.name] = param
+
+    def get(self, name):
+        if not self.has(name):
+            raise Exception("Parameter does not exist: {}".format(name))
+        return self.parameters[name]
+
+    def has(self, name):
+        return name in list(self.parameters.keys())
+
+    def __str__(self):
+        return str([p.to_dict() for p in self.parameters.values()])
+
+    def load_defaults(self):
+        for param in self.parameters.values():
+            param.set_value_from_default()
+
+    def load_from_dict(self, json_dict):
+        for k,v in json_dict.items():
+            if self.has(k):
+                p = self.get(k)
+                if p.read_from_dict:
+                    self.get(k).set_value(v)
+                    #print('load from dict: {} = {}'.format(k, v))
+
+    def load_from_args(self, **kwargs):
+        #print('load from args: {}'.format(kwargs))
+        for k,v in kwargs.items():
+            if self.has(k):
+                p = self.get(k)
+                #print('found param: {}'.format(k))
+                if p.read_from_args:
+                    p.set_value(v)
+                    #print('load from args: {} = {}'.format(k, v))
+
+    def validate(self):
+        return len(self.get_invalid_parameters()) > 0
+
+    def get_invalid_parameters(self):
+        invalid_parameters = []
+        for p in self.parameters.values():
+            if p.invalid():
+                invalid_parameters.append(p)
+        return invalid_parameters
+
+class DummyParameterObject:
+
+    def __init__(self, **kwargs):
+
+        self.parameters = ParameterSet(Parameter     ('myparam',     'a param',          optional=False, value='foobarbaz'),
+                                       FloatParameter('float',       'a float',          optional=True,  default_value=123.4),
+                                       IntParameter  ('int',         'an int',           optional=True,  value=1234),
+                                       BoolParameter ('bool',        'a bool',           optional=True,  value=True, default_value=False),
+                                       BoolParameter ('boolfromstr', 'a bool from str',  optional=True,  value='False'),
+                                       IntParameter  ('intfromstr',  'an int from str',  optional=True,  value='1234'),
+                                       Parameter     ('required',    'a required param', optional=False, default_value='blerp'))
+
+        self.parameters.load_from_args(**kwargs)
+
+if __name__ == '__main__':
+
+    dummy_obj = DummyParameterObject(myparam='dingus')
+    params = dummy_obj.parameters
+
+    print("Initial parameters...")
+    print(params)
+
+    params.load_defaults()
+
+    example_dict = {'myparam': 'bongle',
+                    'float': 5.678,
+                    'int': 5678,
+                    'bool': False,
+                    'boolfromstr': 'True',
+                    'intfromstr': '5678'}
+    params.load_from_dict(example_dict)
+
+    print("Parameters after load...")
+    print(params)
+
+
+    params.validate()
