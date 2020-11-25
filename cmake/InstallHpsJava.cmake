5d0f669f6ed9c0ec11287b31982925d319782b31
Jeremy McCormick <jermccormick@gmail.com>
2020-11-24 19:00:59 -0600

    Change options so external project installation is off by default.
    Add option for specifying the HPS Java tag and figuring out the version.

--- cmake/InstallHpsJava.cmake
+++ cmake/InstallHpsJava.cmake
@@ -1,9 +1,17 @@
 set(HPSJAVA              "HpsJava")
-set(HPSJAVA_TAG          master)
-set(HPSJAVA_VERSION      4.5-SNAPSHOT)
-set(HPSJAVA_JAR_NAME     hps-distribution-${HPSJAVA_VERSION}-bin.jar)
+if(NOT DEFINED HPSJAVA_TAG)
+    set(HPSJAVA_TAG      "master")
+endif()
 set(HPSJAVA_INSTALL_DIR  ${CMAKE_INSTALL_PREFIX}/lib)
 set(HPSJAVA_BUILD_DIR    ${CMAKE_BINARY_DIR}/${HPSJAVA})
+set(HPSJAVA_POM_URL      https://raw.githubusercontent.com/JeffersonLab/hps-java/${HPSJAVA_TAG}/pom.xml)
+set(HPSJAVA_POM_LOCAL    ${CMAKE_BINARY_DIR}/${HPSJAVA}_pom.xml)
+
+file(DOWNLOAD ${HPSJAVA_POM_URL} ${HPSJAVA_POM_LOCAL})
+message(STATUS "Downloading ${HPSJAVA} POM file from: ${HPSJAVA_POM_URL}")
+execute_process(COMMAND ${CMAKE_SOURCE_DIR}/scripts/print_hps_java_version.sh ${HPSJAVA_POM_LOCAL} OUTPUT_VARIABLE HPSJAVA_VERSION)
+
+set(HPSJAVA_JAR_NAME     hps-distribution-${HPSJAVA_VERSION}-bin.jar)
 set(HPSJAVA_BIN_JAR      ${HPSJAVA_INSTALL_DIR}/${HPSJAVA_JAR_NAME})
 
 externalproject_add(
@@ -24,4 +32,6 @@ externalproject_add(
 
 add_dependencies(external ${HPSJAVA})
 
+message(STATUS "Using HPS Java tag: ${HPSJAVA_TAG}")
+message(STATUS "Using HPS Java version: ${HPSJAVA_VERSION}")
 message(STATUS "${HPSJAVA} bin jar will be installed to: ${HPSJAVA_BIN_JAR}")
