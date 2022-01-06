f7e3693384795db815fe939355aa9d1bc2f95a90
Jeremy McCormick <jeremym@slac.stanford.edu>
2022-01-06 17:53:09 -0600

    Update with new option names

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
+#!/bin/bash
+
 (
     if [ -z "$GSL_ROOT_DIR" ]; then
         echo "ERROR: Missing GSL_ROOT_DIR in environment"
         exit 1
     fi
-    mkdir -p build && cd build && \
+    rm -rf build &> /dev/null
+    mkdir -p build && cd build
     cmake .. \
         -DCMAKE_INSTALL_PREFIX=$(realpath ../install) \
         -DPython3_EXECUTABLE=`which python3` \
         -DGSL_ROOT_DIR=${GSL_ROOT_DIR} \
         -DCMAKE_BUILD_TYPE=RelWithDbInfo \
         -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) \
-        -DENABLE_INSTALL_GENERATORS=ON \
-        -DENABLE_INSTALL_FIELDMAPS=ON \
-        -DENABLE_INSTALL_LCIO=ON \
-        -DENABLE_INSTALL_HPSJAVA=ON \
-        -DENABLE_INSTALL_CONDITIONS=OFF \
-        -DHPSJAVA_TAG=master && \
+        -DHPSJAVA_TAG=master \
+        -DHPSMC_ENABLE_EGS5=ON \
+        -DHPSMC_ENABLE_MADGRAPH=ON \
+        -DHPSMC_ENABLE_STDHEP=ON \
+        -DHPSMC_ENABLE_FIELDMAPS=ON \
+        -DHPSMC_ENABLE_LCIO=ON \
+        -DHPSMC_ENABLE_HPSJAVA=ON \
+        -DHPSMC_ENABLE_CONDITIONS=OFF
     make install
 )
