87f67267e86e4348f4bdbb8182e2183790684f74
Jeremy McCormick <jeremym@slac.stanford.edu>
2022-02-04 19:56:21 -0500

    Add HPSTR output

--- python/jobs/tritrig_sim_full_chain_job.py
+++ python/jobs/tritrig_sim_full_chain_job.py
@@ -1,5 +1,5 @@
 from hpsmc.generators import MG5, StdHepConverter
-from hpsmc.tools import Sim, JobManager, FilterBunches, BeamCoords, AddMother
+from hpsmc.tools import Sim, JobManager, FilterBunches, BeamCoords, AddMother, HPSTR
 
 job.description = 'tritrig signal generation to recon using hps-sim with no beam backgrounds'
 
@@ -7,7 +7,7 @@ job.description = 'tritrig signal generation to recon using hps-sim with no beam
 mg = MG5(name='tritrig')
 
 # Convert LHE output to stdhep
-cnv = StdHepConverter()
+stdhep_cnv = StdHepConverter()
 
 # Add mother particle to tag trident particles
 mom = AddMother()
@@ -30,11 +30,18 @@ readout = JobManager(steering='readout')
 # Run physics reconstruction
 recon = JobManager(steering='recon')
 
+# Convert LCIO to ROOT
+root_cnv = HPSTR(cfg='recon')
+
+# Run an analysis on the ROOT file
+ana = HPSTR(cfg='ana')
+
 # Set persistency tags for output files
 job.ptag('gen', 'tritrig_unweighted_events_mom_rot.stdhep')
 job.ptag('sim', 'tritrig_unweighted_events_mom_rot.slcio')
 job.ptag('readout', 'tritrig_unweighted_events_mom_rot_filt_readout.slcio')
 job.ptag('recon', 'tritrig_unweighted_events_mom_rot_filt_readout_recon.slcio')
+job.ptag('ana', 'tritrig_unweighted_events_mom_rot_filt_readout_recon_ana.root')
  
 # Add job components
-job.add([mg, cnv, mom, rot, sim, filter_bunches, readout, recon])
+job.add([mg, stdhep_cnv, mom, rot, sim, filter_bunches, readout, recon, root_cnv, ana])
