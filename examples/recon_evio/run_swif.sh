cb02c136bfac910ae8334a46ae31068f414aed62
tongtongcao <tongtongcao1@gmail.com>
2022-05-19 21:06:57 -0400

    slight changes

--- examples/recon_evio/run_swif.sh
+++ examples/recon_evio/run_swif.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-hps-mc-batch swif -c /u/group/hps/users/caot/hps-mc/config/jlab_tongtong.cfg  -c .hpsmc -D -W 4 -w recon_evio_run14750 -l /w/hallb-scshelf2102/hps/caot/experimentData/2021/recon/HPS_Run2021Pass2FEE/hps_014750/PhysicsRun2021_pass0_recon_evio/logs recon_evio jobs.json
+hps-mc-batch swif -c /u/group/hps/users/caot/hps-mc/config/jlab_tongtong.cfg  -c .hpsmc -W 24 -l /w/hallb-scshelf2102/hps/caot/experimentData/2021/recon/HPS_Run2021Pass2FEE/hps_014750/PhysicsRun2021_pass0_recon_evio/logs recon_evio jobs.json
