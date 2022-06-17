268c05fecedf15f5ccb63f8c089dc1b602dc54f7
tongtongcao <tongtongcao1@gmail.com>
2022-06-17 11:01:57 -0400

    add a job script for FEE from generation to reconstruction

---
+++ python/jobs/fee_gen_to_recon_job.py
@@ -0,0 +1,36 @@
+from hpsmc.generators import EGS5
+from hpsmc.tools import BeamCoords, RandomSample, SLIC, ExtractEventsWithHitAtHodoEcal, JobManager
+
+job.description = 'beam from generation to slic'
+
+# Get job input file targets
+inputs = list(job.input_files.values())
+
+if 'nevents' in job.params:
+    nevents = job.params['nevents']
+else:
+    nevents = 250000
+
+# Generate beam
+egs5 = EGS5(name="beam_v7_%s" % job.params['run_params'])
+
+# Rotate events into beam coordinates
+rot = BeamCoords()
+
+# Sample events into new stdhep file
+sample = RandomSample()
+
+# Simulate events
+slic = SLIC(nevents=nevents+1)
+
+# Space signal events
+space_events = ExtractEventsWithHitAtHodoEcal(event_interval=250, num_hodo_hits=0)
+
+# Run simulated events in readout to generate triggers
+readout = JobManager(steering='readout')
+
+# Run physics reconstruction
+recon = JobManager(steering='recon')
+
+# Run the job
+job.add([egs5, rot, sample, slic, space_events, readout, recon])
