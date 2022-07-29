2816afc87ec783a0d6afaaea75d66f71dc5657f4
sgaiser <sgaiser@stanford.edu>
2022-07-29 16:28:12 -0700

    Doxygen style comments in tools/stdhep-tools + formatting of code

--- tools/stdhep-tools/src/merge_poisson.cc
+++ tools/stdhep-tools/src/merge_poisson.cc
@@ -9,10 +9,12 @@
 
 #include <unistd.h>
 
-// takes input stdhep files, merges a Poisson-determined number of events per event into a new stdhep file
+/**
+ * Takes input stdhep files, merges a Poisson-determined number of events per event into a new stdhep file.
+ */
 int main(int argc,char** argv)
 {
-	int nevhep;             /* The event number */
+	int nevhep;             //!< The event number
 	vector<stdhep_entry> new_event;
 
 	double poisson_mu = -1.0;
@@ -26,7 +28,7 @@ int main(int argc,char** argv)
 
 	int c;
 
-	while ((c = getopt(argc,argv,"hn:m:N:s:O:")) !=-1)
+	while ((c = getopt(argc,argv,"hn:m:N:s:O:")) != -1)
 		switch (c)
 		{
 			case 'h':
@@ -61,9 +63,9 @@ int main(int argc,char** argv)
 				abort();
 		}
 
-	printf("Mean events per output event %f, output events per output file %d, max output files %d\n",poisson_mu,output_n,max_output_files);
+	printf("Mean events per output event %f, output events per output file %d, max output files %d\n", poisson_mu, output_n, max_output_files);
 
-	if ( argc-optind <2 )
+	if (argc - optind < 2)
 	{
 		printf("<input stdhep filenames> <output stdhep basename>\n");
 		return 1;
@@ -76,57 +78,55 @@ int main(int argc,char** argv)
 
 	T = gsl_rng_mt19937;
 	r = gsl_rng_alloc (T);
-	gsl_rng_set(r,rseed);
-
+	gsl_rng_set(r, rseed);
 
 	int n_events;
 	int istream = 0;
 	int ostream = 1;
 	int ilbl;
 
-
-	if (max_output_files!=0&&poisson_mu<0) {
+	if (max_output_files != 0 && poisson_mu < 0) {
 		int evcount = 0;
-		open_read(argv[optind],istream);
+		open_read(argv[optind], istream);
 		while (read_next(istream)) {
 			evcount++;
 		}
 		close_read(istream);
-		printf("Counted %d events\n",evcount);
+		printf("Counted %d events\n", evcount);
 
 		poisson_mu = (double) evcount/(output_n*max_output_files*poisson_mu);
-		printf("set mu = %f\n",poisson_mu);
+		printf("set mu = %f\n", poisson_mu);
 	}
 
-	if (poisson_mu<0) poisson_mu*=-1;
+	if (poisson_mu < 0) poisson_mu *= -1;
 
 	char *output_basename = argv[argc-1];
 	char output_filename[100];
 	int file_n = 1;
 
-	open_read(argv[optind++],istream);
-	while (max_output_files==0||file_n-1<max_output_files) {
+	open_read(argv[optind++], istream);
+	while (max_output_files == 0 || file_n - 1 < max_output_files) {
 		bool write_this_file = true;
-		if (only_this_file!=0 && file_n!=only_this_file) {
-			printf("skip file %d\n",file_n);
+		if (only_this_file != 0 && file_n != only_this_file) {
+			printf("skip file %d\n", file_n);
 			write_this_file = false;
 		}
-		sprintf(output_filename,"%s_%0*d.stdhep",output_basename,output_filename_digits,file_n);
+		sprintf(output_filename, "%s_%0*d.stdhep", output_basename, output_filename_digits, file_n);
 		if (write_this_file)
-			open_write(output_filename,ostream,output_n);
+			open_write(output_filename, ostream, output_n);
 		for (int nevhep = 0; nevhep < output_n; nevhep++)
 		{
-			int n_merge = gsl_ran_poisson(r,poisson_mu);
-			if (n_merge==0 && write_this_file)
+			int n_merge = gsl_ran_poisson(r, poisson_mu);
+			if (n_merge == 0 && write_this_file)
 				add_filler_particle(&new_event);
 			//if (n_merge!=0) printf("file %d, nevhep %d: n_merge %d\n",file_n,nevhep,n_merge);
-			for (int i=0;i<n_merge;i++)
+			for (int i = 0; i < n_merge; i++)
 			{
 				while (!read_next(istream)) {
 					close_read(istream);
-					if (optind<argc-1)
+					if (optind < argc - 1)
 					{
-						open_read(argv[optind++],istream);
+						open_read(argv[optind++], istream);
 					}
 					else
 					{
@@ -142,7 +142,7 @@ int main(int argc,char** argv)
 			}
 
 			if (write_this_file) {
-				write_stdhep(&new_event,nevhep+1);
+				write_stdhep(&new_event, nevhep+1);
 				write_file(ostream);
 			}
 		}
--- tools/stdhep-tools/src/split.cc
+++ tools/stdhep-tools/src/split.cc
@@ -4,46 +4,48 @@
 #include <stdhep.h>
 #include <string.h>
 
-// Reads one stdhep file and splits it into multiple files, each of a given length.
-int main(int argc,char** argv)
+/**
+ * Reads one stdhep file and splits it into multiple files, each of a given length.
+ */
+int main(int argc, char** argv)
 {
 	char outputname[200];
-	if (argc!=4) 
+	if (argc != 4) 
 	{
 		printf("<input stdhep filename> <output stdhep basename> <number of events per file>\n");
 		return 1;
 	}
 	int n_events = atoi(argv[3]);
-	printf("Reading %d events from %s\n",n_events,argv[1]);
+	printf("Reading %d events from %s\n", n_events, argv[1]);
 	int istream = 0;
 	int ostream = 1;
 	int ilbl;
-	StdHepXdrReadInit(argv[1],n_events,istream);
+	StdHepXdrReadInit(argv[1], n_events, istream);
 	int j = 1;
 
 	while (true) {
-		sprintf(outputname,"%s_%d.stdhep",argv[2],j);
-		printf("Writing to %s\n",outputname);
-		StdHepXdrWriteOpen(outputname,outputname,n_events,ostream);
-		StdHepXdrWrite(100,ostream);
+		sprintf(outputname, "%s_%d.stdhep", argv[2], j);
+		printf("Writing to %s\n", outputname);
+		StdHepXdrWriteOpen(outputname, outputname, n_events, ostream);
+		StdHepXdrWrite(100, ostream);
 
-		for (int i=0;i<n_events;i++)
+		for (int i = 0; i < n_events; i++)
 		{
 			do {
-				if (StdHepXdrRead(&ilbl,istream)!=0) {
+				if (StdHepXdrRead(&ilbl, istream) != 0) {
 					printf("End of file\n");
-					printf("Last output file contains %d events\n",n_events);
+					printf("Last output file contains %d events\n", n_events);
 					StdHepXdrEnd(istream);
-					StdHepXdrWrite(200,ostream);
+					StdHepXdrWrite(200, ostream);
 					StdHepXdrEnd(ostream);
 					return(0);
 				}
-				if (ilbl!=1)
-					printf("ilbl = %d\n",ilbl);
-			} while (ilbl!=1);
-			StdHepXdrWrite(ilbl,ostream);
+				if (ilbl != 1)
+					printf("ilbl = %d\n", ilbl);
+			} while (ilbl != 1);
+			StdHepXdrWrite(ilbl ,ostream);
 		}
-		StdHepXdrWrite(200,ostream);
+		StdHepXdrWrite(200, ostream);
 		StdHepXdrEnd(ostream);
 		j++;
 	}
