80c9eff7343f63f20417eb2a0d447b2e1730f7d2
sgaiser <sgaiser@stanford.edu>
2022-09-05 12:25:55 -0700

    fixed Doxygen pages generation

---
+++ mainpage.h
@@ -0,0 +1,8 @@
+/**
+ * \mainpage Heavy Photon Search Toolkit for Reconstruction
+ * 
+ * \section documentation Documentation
+ * 
+ * Example documentation can be found at \subpage examples "examples".
+ * 
+ */
