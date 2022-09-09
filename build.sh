43b0530afb1ae73e6af371463eb09d671193dc2c
sgaiser <sgaiser@stanford.edu>
2022-09-09 15:20:00 -0700

    some cleanup

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 
 (
     rm -rf build && mkdir -p build && cd build
-    /sdf/group/hps/users/sgaiser/src/cmake/3.22/bin/cmake .. \
+    cmake .. \
         -DPython3_EXECUTABLE=$(which python3) \
         -DCMAKE_BUILD_TYPE=RelWithDbInfo \
         -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) \
-        -DHPSMC_ENABLE_EGS5=OFF \
-        -DHPSMC_ENABLE_MADGRAPH=OFF \
+        -DHPSMC_ENABLE_EGS5=ON \
+        -DHPSMC_ENABLE_MADGRAPH=ON \
         -DHPSMC_ENABLE_STDHEP=ON \
-        -DHPSMC_ENABLE_FIELDMAPS=OFF \
-        -DHPSMC_ENABLE_LCIO=OFF \
-        -DHPSMC_ENABLE_HPSJAVA=OFF \
+        -DHPSMC_ENABLE_FIELDMAPS=ON \
+        -DHPSMC_ENABLE_LCIO=ON \
+        -DHPSMC_ENABLE_HPSJAVA=ON \
         -DHPSMC_ENABLE_CONDITIONS=OFF
     make install
 )
