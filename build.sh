483dc0c799e030142eeca54d3f0d940ad188d589
Cameron Bravo <bravo@slac.stanford.edu>
2022-12-01 11:49:48 -0800

    Use CMake to configure ROOT location for jlab env script.

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@
         -DPython3_EXECUTABLE=$(which python3) \
         -DCMAKE_BUILD_TYPE=RelWithDbInfo \
         -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc) \
+        -DHPSMC_ENABLE_ROOT=ON \
         -DHPSMC_ENABLE_EGS5=ON \
         -DHPSMC_ENABLE_MADGRAPH=ON \
         -DHPSMC_ENABLE_STDHEP=ON \
