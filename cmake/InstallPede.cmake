81e0a3b764f53d78ca2869816e26ca6d2e2bbbd5
tomeichlersmith <eichl008@umn.edu>
2023-02-10 13:35:19 -0600

    outline pede Install script

---
+++ cmake/InstallPede.cmake
@@ -0,0 +1,20 @@
+
+externalproject_add("pede"
+    PREFIX            "pede"
+    # download MillepedeII tar-ball from host at JLab
+    #   this also unpacks the tar archive by default
+    URL               "https://hpsweb.jlab.org/test/hps-mc/tars/MillepedeII.tar.gz"
+    URL_MD5           "ab5de8ae6b1b6494899510a6cbc155cd"
+    # have the build commands run from inside source directory
+    BUILD_IN_SOURCE   ON
+    # update install prefix during configure command by editing the Makefile
+    CONFIGURE_COMMAND "sed -i 's|^PREFIX.*$|PREFIX = ${CMAKE_INSTALL_PREFIX}|g' Makefile"
+    # simply run make to build it
+    BUILD_COMMAND     make
+    # simply run make to install it
+    INSTALL_COMMAND   make install
+)
+
+add_dependencies(external ${CONDITIONS})
+externalproject_get_property(${CONDITIONS} INSTALL_DIR)
+message(STATUS "${CONDITIONS} will be installed to: ${INSTALL_DIR}")
