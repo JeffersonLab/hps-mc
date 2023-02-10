b653417109b3835d439107c4ffd55d601cf2d6b2
tomeichlersmith <eichl008@umn.edu>
2023-02-10 13:13:50 -0600

    start outline for PEDE hps-mc component

--- python/hpsmc/tools.py
+++ python/hpsmc/tools.py
@@ -1740,3 +1740,40 @@ class Sim(SimBase):
         @return list of required config
         """
         return ['hps_sim_dir', 'hps_fieldmaps_dir', 'detector_dir']
+
+class PEDE(Component):
+    """! Run pede minimizer over input bin files for alignment
+
+    """
+
+    def __init__(self, **kwargs) :
+        self._pede_steering_file = None
+        super().__init__('pede',
+                command='pede', 
+                **kwargs
+                )
+
+    def _write_pede_steering_file(self) :
+        return '/path/to/pede/steering/file'
+
+    def required_parameters() :
+        return ['inputs']
+
+    def optional_parameters() :
+        return ['subito']
+
+    def cmd_args(self) :
+        a = [self._pede_steering_file]
+        if self.subito :
+            a += ['-s']
+        return a
+
+    def setup(self) :
+        """pre-run initialization"""
+        self._pede_steering_file = _write_pede_steering_file()
+
+    def cleanup(self) :
+        """post-run de-initialization"""
+        # copy pede output files to output directory
+        pass
+
