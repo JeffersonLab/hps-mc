9c5f620450f78ca46d4ca44264c6d10e6164c57e
Sarah Gaiser <s.gaiser96@gmx.de>
2023-02-14 14:28:00 -0800

    added examples for different methods of running jobs

---
+++ examples/run_methods/run_auger.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hps-mc-batch auger -W 4 -D -l $PWD/logs slic jobs.json
---
+++ examples/run_methods/run_slurm.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hps-mc-batch slurm -o -q hps -r 1:1 -E $HPSMC_DIR/bin/hps-mc-env.sh -W 5 -d $HPSMC/examples/run_methods/scratch -c $HPSMC/examples/run_methods/.hpsmc -l $HPSMC/examples/run_methods/scratch/logs beam_gen jobs.json 
