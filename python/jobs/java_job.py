216be8efc3f998156a6361d5380d89785779da93
tomeichlersmith <eichl008@umn.edu>
2023-02-17 15:56:27 -0600

    get to the point of running hps-java, not configured properly tho
    need to do some debugging but the general infrastructure is there

---
+++ python/jobs/java_job.py
@@ -0,0 +1,23 @@
+"""!
+@file java_job.py
+
+Do a single hps-java run with a steering file.
+"""
+
+import os
+import logging
+from hpsmc.tools import JobManager
+
+## Initialize logger with default level
+logger = logging.getLogger('hpsmc.job')
+
+job.description = 'single hps-java run with input steering file'
+
+## Assign ptags for output
+input_files = list(job.input_files.values())
+if len(input_files) > 1:
+    raise Exception('This script accepts only one input file.')
+
+java_run = JobManager(steering = job.params["steering"])
+
+job.add([java_run])
