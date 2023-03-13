0f3b7642e7d78dc74113f7ba56cf87b9ea44fdd7
Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>
2023-03-13 11:26:02 -0500

    rm stale notes from copy of apply job

--- python/jobs/misalign_job.py
+++ python/jobs/misalign_job.py
@@ -3,17 +3,6 @@
 
 Intentionally misalign a detector by changing a set of millepede parameters.
 
-Besides the main purpose of running the pede minimizer, we also do other helpful
-tasks that are commonly done with alignment.
-1. Apply the results from the pede minimizer to a new iteration of the detector
-2. Construct the new iteration of the detector so that another round of tracking
-    can be done
-3. Merge the histogram files generated from the previous round of tracking for
-    later analysis/drawing - this merging is only done if those histogram files
-    exist and an output histogrma file is defined in the job JSON.
-    - The histogram files are looked for by replacing the input _mille.bin suffix
-      on the input files with _gblplots.bin which is the format of the outputs
-      done by the tracking example.
 """
 import os
 from hpsmc.alignment import WriteMisalignedDet, ConstructDetector
