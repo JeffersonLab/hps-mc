1b875985c26ce4cd8a1923556e5b8154d0210d32
Jeremy McCormick <jermccormick@gmail.com>
2023-03-13 18:52:13 -0500

    feat: global logging setup

---
+++ python/hpsmc/_logging.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+def config_logging(stream=sys.stdout, level=logging.DEBUG, logname='hpsmc'):
+    """!
+    Configure logging by setting an output stream and level (both optional).
+    Any handlers already registered will be replaced by calling this method.
+    """
+    # print("Config logging: " + str(stream) + " " + str(level) + " " + logname)
+    logger = logging.getLogger(logname)
+    logger.propagate = False
+    logger.handlers = []  # Reset handlers in case this is called more than once
+    logger.setLevel(level)
+    handler = logging.StreamHandler(stream)
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter('%(name)s:%(levelname)s %(message)s'))
+    logger.addHandler(handler)
+    return logger
