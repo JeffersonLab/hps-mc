cf228dcdd0e384407425578a3fcbab8e016b66cd
Jeremy McCormick <jermccormick@gmail.com>
2023-03-17 18:32:24 -0500

    cleanup: use unix spaces

--- python/hpsmc/_config.py
+++ python/hpsmc/_config.py
@@ -8,6 +8,7 @@ import sys
 import os
 from os.path import expanduser
 
+
 def _read_global_config():
     """! Read global configuration files.
     @param filename  name of json file
@@ -15,6 +16,25 @@ def _read_global_config():
     """
     config = configparser.ConfigParser()
     config_files = config.read([
-        os.path.join(expanduser("~"), ".hpsmc"), 
+        os.path.join(expanduser("~"), ".hpsmc"),
         os.path.abspath(".hpsmc")])
     return config, config_files
+
+def convert_config_value(val):
+    """! Convert config value to Python readable value."""
+    if val == 'True' or val == 'true':
+        return True
+    elif val == 'False' or val == 'false':
+        return False
+    try:
+        if val.contains('.'):
+            floatval = float(val)
+            return floatval
+    except BaseException:
+        pass
+    try:
+        intval = int(val)
+        return intval
+    except BaseException:
+        pass
+    return val
