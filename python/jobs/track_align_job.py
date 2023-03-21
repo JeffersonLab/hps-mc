3d1b62aab864feade8ba67a73bdb89a1b3394d92
tomeichlersmith <eichl008@umn.edu>
2023-03-21 13:04:18 -0500

    WIP: start putting steering parameters into hps-mc

--- python/jobs/track_align_job.py
+++ python/jobs/track_align_job.py
@@ -23,4 +23,38 @@ java_run = JobManager(
     outputs = list(job.output_files.keys())
     )
 
+# update defs to include our track_align defaults for the SimpleGBLTrajAliDriver
+traj_ali_driver_defaults = {
+    'enableAlignmentCuts' : True,
+    'doCOMAlignment' : True,
+    'minMom' : 0.5,
+    'maxMom' : 5.0,
+    'nHitsCut' : 6,
+    'debugAlignmentDs' : False,
+    'correctTrack' : True,
+    'includeNoHitScatters' : False,
+    'gblRefitIterations' : 0,
+    'storeTrackStates' : True,
+    'compositeAlign' : True,
+    'constrainedFit' : False,
+    'momC' : 4.55,
+    'constrainedBSFit' : False,
+    'bsZ' : -7.7,
+    'trackSide' : -1,
+    #'writeMilleBinary' : True,
+    #'milleBinaryFileName' : '${outputFile}.bin',
+    'writeMilleChi2Cut' : 5,
+    'enableStandardCuts' : False,
+    'maxTrackChisq4hits' : 60.,
+    'maxTrackChisq5hits' : 60.,
+    'maxTrackChisq6hits' : 60.,
+    'inputCollectionName' : 'KalmanFullTracks'
+    }
+if java_run.defs is None :
+    java_run.defs = traj_ali_driver_defaults
+else :
+    for key, val in traj_ali_driver_defaults.items() :
+        if key not in java_run.defs :
+            java_run.defs[key] = val
+
 job.add([java_run])
