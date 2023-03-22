84a2d39023c657533b1830a00e76ca96e0534781
Sarah Gaiser <s.gaiser96@gmx.de>
2023-03-22 14:52:57 -0700

    adjust run_slurm example

--- examples/run_methods/run_slurm.sh
+++ examples/run_methods/run_slurm.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-hps-mc-batch slurm -o -q hps -r 1:1 -E $HPSMC_DIR/bin/hps-mc-env.sh -W 5 -d $PWD/scratch -c $PWD/.hpsmc -l $PWD/scratch/logs -S $PWD/scratch slic $PWD/job_slurm.json 
+# Use this hps partition (or other valid partition) for SLAC
+hps-mc-batch slurm -o -q hps -r 1:1 -E $HPSMC_DIR/bin/hps-mc-env.sh -W 5 -d $PWD/scratch -c $PWD/.hpsmc -l $PWD/scratch/logs -S $PWD/scratch slic $PWD/job_slurm.json
+
+# Use this ifarm partition (or other valid partition) for JLab
+# hps-mc-batch slurm -o -q ifarm -r 1:1 -E $HPSMC_DIR/bin/hps-mc-env.sh -W 5 -d $PWD/scratch -c $PWD/.hpsmc -l $PWD/scratch/logs -S $PWD/scratch slic $PWD/job_slurm.json
