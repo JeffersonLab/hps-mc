d6f9c87e5d5d1cda29ce75ac728f35a9b9162b25
tomeichlersmith <eichl008@umn.edu>
2023-04-05 14:02:57 -0500

    run python code styling over developed files

--- python/hpsmc/_hadd.py
+++ python/hpsmc/_hadd.py
@@ -7,10 +7,11 @@ import logging
 
 from hpsmc.component import Component
 
+
 class hadd(Component):
-    """! Run ROOT's histogram/TTree merger 
-    
-    Required: 
+    """! Run ROOT's histogram/TTree merger
+
+    Required:
     - **inputs**: input ROOT files to merge
     - **outputs**: *single* output file to merge into
     Optional:
@@ -19,23 +20,22 @@ class hadd(Component):
 
     logger = logging.getLogger('hpsmc.tools.hadd')
 
-    def __init__(self, **kwargs) :
+    def __init__(self, **kwargs):
         self.output_ext = 'root'
         self.ncores = None
         super().__init__('hadd', command='hadd', **kwargs)
 
-    def optional_parameters(self) :
+    def optional_parameters(self):
         return ['ncores']
 
-    def cmd_args(self) :
+    def cmd_args(self):
         a = []
-        if self.ncores is not None :
-            a += ['-j',self.ncores]
-        if len(self.outputs) != 1 :
+        if self.ncores is not None:
+            a += ['-j', self.ncores]
+        if len(self.outputs) != 1:
             raise ValueError('hadd makes no sense without exactly one output')
         a += self.outputs
-        if self.inputs is None or len(self.inputs) == 0 :
+        if self.inputs is None or len(self.inputs) == 0:
             raise ValueError('hadd makes no sense without at least one input')
         a += self.inputs
         return a
-
--- python/jobs/track_align_job.py
+++ python/jobs/track_align_job.py
@@ -23,45 +23,45 @@ if len(input_files) > 1:
     raise Exception('This script accepts only one input file.')
 
 java_run = JobManager(
-    steering = job.params["steering"],
-    outputs = list(job.output_files.keys())
+    steering=job.params["steering"],
+    outputs=list(job.output_files.keys())
     )
 
 # update defs to include our track_align defaults for the SimpleGBLTrajAliDriver
 traj_ali_driver_defaults = {
-    'enableAlignmentCuts' : True,
-    'doCOMAlignment' : True,
-    'minMom' : 0.5,
-    'maxMom' : 5.0,
-    'nHitsCut' : 6,
-    'debugAlignmentDs' : False,
-    'correctTrack' : True,
-    'includeNoHitScatters' : False,
-    'gblRefitIterations' : 0,
-    'storeTrackStates' : True,
-    'compositeAlign' : True,
-    'constrainedFit' : False,
-    'momC' : 4.55,
-    'constrainedBSFit' : False,
-    'bsZ' : -7.7,
-    'trackSide' : -1,
+    'enableAlignmentCuts': True,
+    'doCOMAlignment': True,
+    'minMom': 0.5,
+    'maxMom': 5.0,
+    'nHitsCut': 6,
+    'debugAlignmentDs': False,
+    'correctTrack': True,
+    'includeNoHitScatters': False,
+    'gblRefitIterations': 0,
+    'storeTrackStates': True,
+    'compositeAlign': True,
+    'constrainedFit': False,
+    'momC': 4.55,
+    'constrainedBSFit': False,
+    'bsZ': -7.7,
+    'trackSide': -1,
     #'writeMilleBinary' : True,
     #'milleBinaryFileName' : '${outputFile}.bin',
-    'writeMilleChisqCut' : 5,
-    'enableStandardCuts' : False,
-    'maxTrackChisqFourHits' : 60.,
-    'maxTrackChisqFiveHits' : 60.,
-    'maxTrackChisqSixHits' : 60.,
-    'inputCollectionName' : 'KalmanFullTracks'
+    'writeMilleChisqCut': 5,
+    'enableStandardCuts': False,
+    'maxTrackChisqFourHits': 60.,
+    'maxTrackChisqFiveHits': 60.,
+    'maxTrackChisqSixHits': 60.,
+    'inputCollectionName': 'KalmanFullTracks'
     }
-if 'defs' not in job.params :
+if 'defs' not in job.params:
     # no user-defined defs
     java_run.defs = traj_ali_driver_defaults
-else :
+else:
     # some user defined defs, make sure others get their defaults
     java_run.defs = job.params['defs']
-    for key, val in traj_ali_driver_defaults.items() :
-        if key not in java_run.defs :
+    for key, val in traj_ali_driver_defaults.items():
+        if key not in java_run.defs:
             java_run.defs[key] = val
 
 job.add([java_run])
