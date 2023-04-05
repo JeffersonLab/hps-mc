990bad214f118176dbe63fa811b7f15f67bc6746
tomeichlersmith <eichl008@umn.edu>
2023-04-05 14:04:12 -0500

    reinstate specialized import

--- python/hpsmc/__init__.py
+++ python/hpsmc/__init__.py
@@ -1,4 +1,3 @@
-from ._hadd import hadd
 import shutil
 
 from ._config import _read_global_config
@@ -22,3 +21,4 @@ else:
     _global_logger.warn("No config files were found at default locations! (`~/.hpsmc` or `.hpsmc` in your current directory)")
 
 # import hadd down here so global_config is defined
+from ._hadd import hadd
