139d362f84950c93cacb9d6658aaff63d41167f2
Jeremy McCormick <jeremym@slac.stanford.edu>
2023-05-18 15:01:21 -0400

    Make specific slurm example scripts for slac and jlab

---
+++ examples/run_methods/run_slurm_slac.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Use this hps partition (or other valid partition) for SLAC
+hps-mc-batch slurm -o -q hps -r 1:1 -E $HPSMC_DIR/bin/hps-mc-env.sh -W 5 -d $PWD/scratch -c $PWD/.hpsmc -l $PWD/scratch/logs -S $PWD/scratch slic $PWD/job_slurm.json
