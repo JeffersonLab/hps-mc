e73c7e821381b0e13a3fe53d71d6527c834b00de
Jeremy McCormick <jeremym@slac.stanford.edu>
2023-05-23 14:56:45 -0400

    Use a sensible run directory for Slurm example at JLab

--- examples/run_methods/run_slurm_jlab.sh
+++ examples/run_methods/run_slurm_jlab.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Use this ifarm partition (or other valid partition) for JLab
-hps-mc-batch slurm -o -q ifarm -r 1:1 -W 5 -d $PWD/scratch -c $PWD/.hpsmc -l $PWD/scratch/logs -S $PWD/scratch slic $PWD/job_slurm.json
+hps-mc-batch slurm -o -q ifarm -r 1:1 -W 5 -d /volatile/hallb/hps/$USER/hpsmc_test -c $PWD/.hpsmc -l $PWD/logs slic $PWD/job_slurm.json
