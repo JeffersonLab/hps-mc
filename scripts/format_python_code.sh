3ea8c9f53685eaf29526492a803774a6e09e75f6
Jeremy McCormick <jermccormick@gmail.com>
2023-05-23 15:37:32 -0400

    Execute formatting properly so it finds the right python

--- scripts/format_python_code.sh
+++ scripts/format_python_code.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-autopep8 --aggressive --aggressive --ignore=E265,E266,E501,E121,E123,E126,E133,E226,E241,E242,E704,W503,W504,W505 --in-place --max-line-length=160 --recursive python/
+python3 -m autopep8 --aggressive --aggressive --ignore=E265,E266,E501,E121,E123,E126,E133,E226,E241,E242,E704,W503,W504,W505 --in-place --max-line-length=160 --recursive python/
