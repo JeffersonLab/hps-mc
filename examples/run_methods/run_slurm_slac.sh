89872cb575b893dca520ce2b5b3bed2dafec016c
Jeremy McCormick <jermccormick@gmail.com>
2023-05-24 16:15:45 -0700

    Use a generic slurm example script

--- examples/run_methods/run_slurm_slac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# This uses the 'shared' partition which could be changed to 'hps' if the user has access.
-hps-mc-batch slurm -q shared -r 1:1 -W 1 -c $PWD/.hpsmc slic $PWD/job_slurm.json
