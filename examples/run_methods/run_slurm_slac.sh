11dae3b79cf5449c6ce656d536015051a731d2b5
Jeremy McCormick <jermccormick@gmail.com>
2023-05-24 13:16:19 -0700

    Simplify this example script to use defaults

--- examples/run_methods/run_slurm_slac.sh
+++ examples/run_methods/run_slurm_slac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # This uses the 'shared' partition which could be changed to 'hps' if the user has access.
-hps-mc-batch slurm -q shared -r 1:1 -W 5 -c $PWD/.hpsmc -l $PWD/scratch/logs -S $PWD/scratch slic $PWD/job_slurm.json
+hps-mc-batch slurm -q shared -r 1:1 -W 1 -c $PWD/.hpsmc slic $PWD/job_slurm.json
