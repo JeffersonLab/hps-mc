ddf025113b561a5ebb3adc2e0843f2cb5c3b385e
Jeremy McCormick <jermccormick@gmail.com>
2023-05-24 16:15:45 -0700

    Use a generic slurm example script

---
+++ examples/run_methods/run_slurm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run a slurm job using mostly default arguments.
+hps-mc-batch slurm slic job_slurm.json
--- examples/run_methods/run_slurm_jlab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Use this ifarm partition (or other valid partition) for JLab
-hps-mc-batch slurm -o -q ifarm -r 1:1 -W 5 -d /volatile/hallb/hps/$USER/hpsmc_test -c $PWD/.hpsmc -l $PWD/logs slic $PWD/job_slurm.json
