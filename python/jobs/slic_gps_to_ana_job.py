cdf2e735cd9e7df71b5e48cbd78e14fcc07bba1d
Abhisek Datta <abhisek.datta@cern.ch>
2023-10-02 11:34:26 -0700

    dont use nevents for hps-java readout

--- python/jobs/slic_gps_to_ana_job.py
+++ python/jobs/slic_gps_to_ana_job.py
@@ -17,7 +17,7 @@ sim = SLIC(inputs=[input_filename])
 filter_bunches = FilterBunches()
 
 ## Run simulated events in readout to generate triggers
-readout = JobManager(steering='readout')
+readout = JobManager(steering='readout', ignore_job_params=['nevents'])
 
 ## Run physics reconstruction
 recon = JobManager(steering='recon')
