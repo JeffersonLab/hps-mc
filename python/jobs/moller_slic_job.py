6ffaf9611dda7faa7b420bca5b60d692f177de43
Sarah Gaiser <sgaiser@stanford.edu>
2025-01-08 09:12:14 -0800

    initial setup

---
+++ python/jobs/moller_slic_job.py
@@ -0,0 +1,31 @@
+"""!
+@file slic_job.py
+Simulation of signals in detector using SLIC.
+"""
+import os
+from hpsmc.tools import SLIC
+job.description = 'detector sim via slic'
+
+## Get job input file targets
+inputs = list(job.input_files.values())
+
+## get file names
+output_names = []
+
+for i in range(len(inputs)):
+    filename, file_extension = os.path.splitext(inputs[i])
+    outname = filename + '.slcio'
+    output_names.append(outname)
+
+if 'nevents' in job.params:
+    nevents = job.params['nevents']
+else:
+    nevents = 250000
+
+## Simulate events
+slic_outs = []
+for i in range(len(inputs)):
+    slic_outs.append(SLIC(inputs=[inputs[i]], outputs=[output_names[i]], nevents=nevents + 1))
+
+## Run the job
+job.add(slic_outs)
---
+++ python/jobs/recon_job.py
@@ -0,0 +1,40 @@
+"""!
+@file readout_recon_job.py
+Simulate pile-up, run readout, hps-java recon, and analysis.
+"""
+import os
+import logging
+from hpsmc.tools import EvioToLcio, JobManager, FilterBunches, LCIOCount, HPSTR
+
+## Initialize logger with default level
+logger = logging.getLogger('hpsmc.job')
+job.description = 'Simulate pile-up, run readout, hps-java recon, and analysis'
+
+## Assign ptags for output
+input_files = list(job.input_files.values())
+if len(input_files) > 1:
+    raise Exception('This script accepts only one input file.')
+
+output_base = os.path.splitext(os.path.basename(input_files[0]))[0]
+job.ptag('filt', '%s_filt.slcio' % output_base)
+job.ptag('readout', '%s_filt_readout.slcio' % output_base)
+job.ptag('lcio_recon', '%s_filt_readout_recon.slcio' % output_base)
+job.ptag('hpstr_recon', '%s_filt_readout_recon.root' % output_base)
+job.ptag('hpstr_ana', '%s_filt_readout_recon_ana.root' % output_base)
+
+datacnv = EvioToLcio(steering='cnv')
+
+## Run physics reconstruction
+#reco = JobManager(steering='recon')
+reco = EvioToLcio(steering='recon')
+
+count_reco = LCIOCount()
+
+## Convert LCIO to ROOT
+cnv = HPSTR(cfg='cnv')
+
+## Run an analysis on the ROOT file
+ana = HPSTR(cfg='ana')
+
+#job.add([ana])
+job.add([reco, count_reco, cnv, ana])
