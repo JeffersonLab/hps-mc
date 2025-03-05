146fb43f8c6f0d682f1ed888a3175e1a878047f1
tongtongcao <tongtongcao1@gmail.com>
2025-03-05 11:05:23 -0500

    change default OS for JLab batch mode

--- python/hpsmc/batch.py
+++ python/hpsmc/batch.py
@@ -577,7 +577,7 @@ class Auger(BatchSystem):
         if self.os is not None:
             auger_os = self.os
         else:
-            auger_os = 'general'
+            auger_os = 'el9'
         os_elem.set("name", auger_os)
         return req
 
