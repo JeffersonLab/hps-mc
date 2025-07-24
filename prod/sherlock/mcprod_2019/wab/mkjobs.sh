1a0195001e8c23d05f9413f6ded39c8f8140f85d
mghrear <mghrear@stanford.edu>
2025-07-24 12:06:11 -0700

    cleanup

--- prod/sherlock/mcprod_2019/wab/mkjobs.sh
+++ prod/sherlock/mcprod_2019/wab/mkjobs.sh
@@ -1,1 +1,1 @@
-hps-mc-job-template -j 1 -r 10000 -a vars.json job.json.tmpl jobs.json
+hps-mc-job-template -j 1 -r 1000 -a vars.json job.json.tmpl jobs.json
--- prod/sherlock/mcprod_2019/wab/run.sh
+++ prod/sherlock/mcprod_2019/wab/run.sh
@@ -1,1 +1,1 @@
-hps-mc-job run -d $PWD/scratch wab_gen_to_slic job.json
+hps-mc-job run -d /home/groups/laurenat/majd/scratch/wab wab_gen_to_slic job.json
