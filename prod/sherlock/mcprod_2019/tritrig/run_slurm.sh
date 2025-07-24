98574e5e06a08512e25b310554b8d05c48ae4399
mghrear <mghrear@stanford.edu>
2025-07-23 21:40:48 -0700

    making sherlock dir

---
+++ prod/sherlock/mcprod_2019/tritrig/run_slurm.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export JOBDIR=/home/groups/laurenat/majd/HPS/hps-mc/prod/slac/mcprod_2019/tritrig
+export RUNDIR=/home/groups/laurenat/majd/scratch/tritrig
+
+hps-mc-batch slurm -o -r 1:100 -E /home/groups/laurenat/majd/HPS/hps-mc/install/bin/hps-mc-env.sh -W 9 -q normal -d $RUNDIR  -c $JOBDIR/.hpsmc tritrig_gen_to_slic $JOBDIR/jobs.json 
+
---
+++ prod/sherlock/mcprod_2019/tritrig/signal_pulser_overlay_to_recon/mkjobs.sh
@@ -0,0 +1,1 @@
+hps-mc-job-template -i tritrig tritrig_files.txt 5 -i pulser pulser_files.txt 1 -j 1 -m 20 -a vars.json job.json.tmpl jobs.json
