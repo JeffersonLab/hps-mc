4fb1465a0211c347938a3ba2a6bf3b6c9068110f
Matthew Gignac <mgignac@slac.stanford.edu>
2026-01-14 11:07:56 -0800

    update CMake configuration and README to be in sync

--- cmake/InstallFieldmaps.cmake
+++ cmake/InstallFieldmaps.cmake
@@ -1,21 +1,17 @@
+set(FIELDMAP_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/fieldmap")
+
 externalproject_add(
     Fieldmaps
-    INSTALL_DIR       "${CMAKE_INSTALL_PREFIX}/share/fieldmap"
     GIT_REPOSITORY    "https://github.com/JeffersonLab/hps-fieldmaps"
     GIT_SHALLOW       ON
     SOURCE_DIR        ${CMAKE_BINARY_DIR}/Fieldmaps
     BUILD_COMMAND     cd ${CMAKE_BINARY_DIR}/Fieldmaps && ./unzip.sh
     UPDATE_COMMAND    git pull
-    INSTALL_COMMAND   ""
+    INSTALL_COMMAND   ${CMAKE_COMMAND} -E make_directory ${FIELDMAP_INSTALL_DIR}
+              COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_BINARY_DIR}/Fieldmaps ${FIELDMAP_INSTALL_DIR}
     CONFIGURE_COMMAND ""
 )
 
 add_dependencies(external Fieldmaps)
 
-externalproject_get_property(Fieldmaps SOURCE_DIR)
-externalproject_get_property(Fieldmaps INSTALL_DIR)
-
-file(GLOB_RECURSE FIELDMAP_FILES "${SOURCE_DIR}/*.dat")
-install(FILES ${FIELDMAP_FILES} DESTINATION ${INSTALL_DIR})
-
-message(STATUS "Fieldmaps will be installed to: ${INSTALL_DIR}")
+message(STATUS "Fieldmaps will be installed to: ${FIELDMAP_INSTALL_DIR}")
