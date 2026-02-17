dd683e925b10b6f6ea0987657d72ddfa0eded8c6
mghrear <mghrear@stanford.edu>
2026-02-17 10:43:04 -0800

    adding v9 pass5

--- prod/sherlock/mcprod_2019/tritrig/sig_to_recon/mkjobs.sh
+++ prod/sherlock/mcprod_2019/tritrig/sig_to_recon/mkjobs.sh
@@ -1,1 +1,1 @@
-hps-mc-job-template -i tritrig tritrig_files.txt 5 -j 1 -m 20 -a vars.json job.json.tmpl jobs.json
+hps-mc-job-template -i tritrig tritrig_files.txt 25 -j 1 -m 40 -a vars.json job.json.tmpl jobs.json
---
+++ prod/sherlock/mcprod_2021/FakeGen_0pt8_pion/signal_pulser_overlay_to_recon/run.sh
@@ -0,0 +1,1 @@
+hps-mc-job run -d $PWD/scratch signal_pulser_slcio_overlay_to_recon job.json
